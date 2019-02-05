12bf0882ca6c963759f21ddefacb9a084c6ebc80
Lane Campbell <ldc0618@gmail.com>
2019-02-04 18:15:55 -0800

    add front end project, rename client to backend

---
+++ backend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+  test = '<h1>injected</h1>';
+}
