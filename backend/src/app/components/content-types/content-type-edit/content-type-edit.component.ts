ecc4d9436174db054d54cdd88884cc50a7d851cd
Lane Campbell <ldc0618@gmail.com>
2019-02-20 19:36:54 -0800

    add content type edit form

--- backend/src/app/components/content-types/content-type-edit/content-type-edit.component.ts
+++ backend/src/app/components/content-types/content-type-edit/content-type-edit.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 import { switchMap } from "rxjs/operators";
 import { ContentTypesService } from "../../../services/content-types.service";
+import { TextboxQuestion } from "../../../models/question-textbox";
+import { DropdownQuestion } from "../../../models/question-dropdown";
+import { HiddenQuestion } from '../../../models/question-hidden';
 
 @Component({
   selector: "app-content-type-edit",
@@ -16,9 +19,55 @@ export class ContentTypeEditComponent implements OnInit {
   ) {}
 
   public contentType;
+  isDataAvailable = false;
+
+  questions = [
+    new HiddenQuestion({
+      key: "contentTypeId",
+      label: "",
+      required: false,
+      order: 1
+    }),
+
+    new HiddenQuestion({
+      key: "fieldId",
+      label: "fieldId",
+      value: "",
+      required: false,
+      order: 2
+    }),
+
+    new TextboxQuestion({
+      key: "systemId",
+      label: "System Id",
+      value: "",
+      required: false,
+      order: 3
+    }),
+
+    new TextboxQuestion({
+      key: "label",
+      label: "Label",
+      value: "my lablel",
+      required: false,
+      order: 4
+    }),
+
+    new DropdownQuestion({
+      key: "isRequired",
+      label: "Required",
+      options: [{ key: "true", value: "true" }, { key: "false", value: "false" }],
+      order: 5
+    }),
+  ];
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get("id");
     this.contentTypesService.getContentType(id);
     this.contentType = this.contentTypesService.contentType;
+    this.isDataAvailable = true;
+  }
+
+  onSubmitContentTypeEdit(){
+
   }
 }
