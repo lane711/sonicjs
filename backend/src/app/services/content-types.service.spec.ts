04022628677e6efc40886b954d61e3bf683deeb4
Lane Campbell <ldc0618@gmail.com>
2019-03-11 20:37:30 -0700

    .

--- backend/src/app/services/content-types.service.spec.ts
+++ backend/src/app/services/content-types.service.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from "@angular/common/http";
+
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { ContentTypesService } from './content-types.service';
 
-// describe('ContentTypesService', () => {
-//   beforeEach(() => TestBed.configureTestingModule({}));
+describe('ContentTypesService', () => {
+  beforeEach(() => 
+  TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+      // , HttpClient
+    ],
+    // declarations: [
+    //   ContentTypesService
+    // ]
+}));
 
-//   it('should be created', () => {
-//     const service: ContentTypesService = TestBed.get(ContentTypesService);
-//     expect(service).toBeTruthy();
-//   });
-// });
+
+  it('should be created', () => {
+    const service: ContentTypesService = TestBed.get(ContentTypesService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should create a new content type', () => {
+    const service: ContentTypesService = TestBed.get(ContentTypesService);
+    let contentType = { name: 'deleteMe'};
+    service.createContentTypeAsync(contentType);
+    expect(service).toBeTruthy();
+  });
+});
