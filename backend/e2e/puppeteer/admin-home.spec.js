53063bf919e48af3f4dc4c4db3200b3f45a0b3e0
Lane Campbell <ldc0618@gmail.com>
2019-03-19 15:12:54 -0700

    mpve puppeter to backend

--- backend/e2e/puppeteer/admin-home.spec.js
+++ backend/e2e/puppeteer/admin-home.spec.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
- 
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+//   console.log(page);
   await page.goto('http://localhost:4200/admin');
-  await page.screenshot({path: 'adminHome.png'});
- 
+  await page.screenshot({path: 'screenshots/admin-home.png'});
+
   await browser.close();
 })();
