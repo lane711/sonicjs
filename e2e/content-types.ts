540217d7b6bac1962350fab4f0acf3b82710c547
Lane Campbell <ldc0618@gmail.com>
2019-03-20 18:59:01 -0700

    automation for new content type

--- e2e/content-types.ts
+++ e2e/content-types.ts
@@ -1,18 +1,26 @@
 const puppeteer = require('puppeteer');
 const shortid = require('shortid');
 const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const { assert } = require('chai'); 
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless:false
+        headless:false,
+        args: [`--window-size=${1080},${680}`] // new option
     });
+    
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1080,
+        height: 680
+    });
+
     await page.goto('http://localhost:4200/admin/content-types');
 
+    //create new content type
     await page.click('.new-content-type');
 
     await page.waitFor('input[id=systemid]');
-
     await page.focus('input[id=systemid]')
     await page.keyboard.type('testSystemId')
 
@@ -24,7 +32,34 @@ const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 
     await page.click('.save');
 
-    //await page.screenshot({path: 'example.png'});
+    //verify new content type
+    let foo = 'bar';
+    assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
+    
+    await page.waitFor('.edit-content-type');
+    const element = await page.$(".edit-content-type");
+    assert.isNotNull(element);
   
-    //await browser.close();
+    //add new field
+    await page.click('.add-field');
+    await page.waitFor('.textBox');
+    await sleep(500)
+    await page.click('.textBox');
+    await page.waitFor('.field-edit-1');
+    await page.click('.field-edit-1');
+
+    //edit field properties
+    await page.focus('input[id=label]')
+    await page.keyboard.type('xXxX')
+    page.$eval('.aside-menu > .save', elem => elem.click());
+
+    // await browser.close();
+
+    
   })();
+
+  function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
+}
