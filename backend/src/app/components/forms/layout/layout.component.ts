85207996a2cd082eec644ddb6ca4d9098cdae534
Lane Campbell <ldc0618@gmail.com>
2019-04-19 11:42:12 -0700

    add layout drop down

--- backend/src/app/components/forms/layout/layout.component.ts
+++ backend/src/app/components/forms/layout/layout.component.ts
@@ -14,12 +14,37 @@ export class LayoutComponent implements OnInit {
   @Input()
   form: FormGroup;
 
+  layout : any;
+  columnOptions : any;
+  layoutColumnOptionsWidth = 300;
+  layoutColumnOptionsHeight = 30;
+
   constructor() { }
 
   ngOnInit() {
-    console.log('layout question', this.question);
-    console.log('layout formGroup', this.form);
+    // console.log('layout question', this.question);
+    // console.log('layout formGroup', this.form);
+    this.loadLayoutColumnOptions();
+    this.processRows();
+  }
 
+  processRows(){
+    this.layout = {};
+    this.layout.rows = [];
+
+    let column = {class : "col"};
+
+    let row = {class : "row", columns : [column, column]};
+    this.layout.rows.push(row);
+    this.layout.rows.push(row);
+
+    console.log('layout', this.layout)
+  }
+
+  loadLayoutColumnOptions(){
+    this.columnOptions = [];
+    this.columnOptions.push(['col']);
+    this.columnOptions.push(['col-3', 'col-9']);
   }
 
 }
