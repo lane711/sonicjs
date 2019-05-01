914b16691b4d4f7702fe1a73264dee67b0dee8f8
Lane Campbell <ldc0618@gmail.com>
2019-04-30 20:24:24 -0700

    add static file support

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,34 +1,34 @@
-var fs = require('fs');
+// var fs = require('fs');
 
-module.exports = function(app) {
-    // Install a "/ping" route that returns "pong"
-  app.get('/ping', function(req, res) {
-    res.send('pong');
-  });
-};
+// module.exports = function(app) {
+//     // Install a "/ping" route that returns "pong"
+//   app.get('/ping', function(req, res) {
+//     res.send('pong');
+//   });
+// };
 
-module.exports = function(app) {
-  var router = app.loopback.Router();
-  router.get('/ping2', function(req, res) {
-      res.send('pongaroo');
-    });
-  app.use(router);
-};
+// module.exports = function(app) {
+//   var router = app.loopback.Router();
+//   router.get('/ping2', function(req, res) {
+//       res.send('pongaroo');
+//     });
+//   app.use(router);
+// };
 
-module.exports = function(app) {
-  var router = app.loopback.Router();
+// module.exports = function(app) {
+//   var router = app.loopback.Router();
 
-  let page = 'my page';
-  fs.readFile( __dirname + '/assets/html/index.html', function (err, data) {
-    if (err) {
-      throw err; 
-    }
-    page = data.toString();
-  });
+//   let page = 'my page';
+//   fs.readFile( __dirname + '/assets/html/index.html', function (err, data) {
+//     if (err) {
+//       throw err; 
+//     }
+//     page = data.toString();
+//   });
 
-  router.get('*', function(req, res) {
-    let url = req.url;
-      res.send(page);
-    }); 
-  app.use(router);
-};
+//   router.get('/', function(req, res) {
+//     let url = req.url;
+//       res.send(page);
+//     }); 
+//   app.use(router);
+// }; 
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
