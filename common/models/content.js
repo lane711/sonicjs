ac41deac9b7fcd7b2261d030ab2ac56086e5e1c4
Lane Campbell <ldc0618@gmail.com>
2019-05-03 20:52:02 -0700

    stub getPageById

--- common/models/content.js
+++ common/models/content.js
@@ -2,4 +2,22 @@
 
 module.exports = function(Content) {
 
+    Content.getPageById = function (cb) {
+        var response = new Date();
+        cb(null, response);
+      };
+    
+      Content.remoteMethod(
+        'getPageById', {
+          http: {
+            path: '/status',
+            verb: 'get',
+          },
+          returns: {
+            arg: 'status',
+            type: 'string',
+          },
+        }
+      );
+
 };
