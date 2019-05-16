6decfe8e0c7e58a3e0951db70a24c3fea1eff166
Lane Campbell <ldc0618@gmail.com>
2019-05-16 09:49:45 -0700

    load quill in jquery

--- backend/src/assets/js/pagebuilder.js
+++ backend/src/assets/js/pagebuilder.js
@@ -1,8 +1,30 @@
 
-$(document).ready(function(){
-  $('#wysiwygModalTrigger').on("click", function (){
-    $('#wysiwygModal').appendTo("body").modal('show');
+$(document).ready(function () {
+
+  var quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        ['bold', 'italic'],
+        ['link', 'blockquote', 'code-block', 'image'],
+        [{ list: 'ordered' }, { list: 'bullet' }]
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'
   });
+
+  $('#wysiwygModalTrigger').on("click", function () {
+    $('#wysiwygModal').appendTo("body").modal('show');
+    $('.ql-editor').text('load me here');
+    console.log('quill', quill);
+  });
+
+  $('.wysiwyg-save').on("click", function () {
+    console.log('saveing wysiwyg');
+    console.log(quill.getContents())
+  });
+
+
 });
 
 
