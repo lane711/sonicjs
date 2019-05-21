670aaa1339e91bf87083885b849d5b01c082930f
Lane Campbell <ldc0618@gmail.com>
2019-05-21 15:48:12 -0700

    quill options

--- backend/src/app/components/page-builder-editor/page-builder-editor.component.ts
+++ backend/src/app/components/page-builder-editor/page-builder-editor.component.ts
@@ -113,14 +113,30 @@ export class PageBuilderEditorComponent implements OnInit {
 
   async loadQuill(){
 
+    var toolbarOptions = [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [ 'link', 'image', 'video', 'formula' ],          // add's image support
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean']                                         // remove formatting button
+  ];
+
     $(document).ready(function () {
       var quill = new Quill('#editor-container', {
         modules: {
-          // toolbar: [
-          //   ['bold', 'italic'],
-          //   ['link', 'blockquote', 'code-block', 'image'],
-          //   [{ list: 'ordered' }, { list: 'bullet' }]
-          // ]
+          toolbar: toolbarOptions
         },
         placeholder: 'Compose an epic...',
         theme: 'snow'
