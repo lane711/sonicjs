43a7e390e4f86975d07968f274ae66f99e3bef95
Lane Campbell <ldc0618@gmail.com>
2019-05-28 19:30:26 -0700

    revert to working button element

--- page-builder/src/app/app.module.ts
+++ page-builder/src/app/app.module.ts
@@ -2,17 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-// import { ButtonComponent } from './button/button.component';
-import { PageBuilderComponent } from './page-builder/page-builder.component'; 
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [PageBuilderComponent],
+  declarations: [ButtonComponent],
   imports: [BrowserModule],
-  entryComponents: [PageBuilderComponent]
+  entryComponents: [ButtonComponent]
 })
 export class AppModule {
   constructor(private injector: Injector) {
-    const customButton = createCustomElement(PageBuilderComponent, { injector });
+    const customButton = createCustomElement(ButtonComponent, { injector });
     customElements.define('custom-button', customButton);
   }
 
---
+++ page-builder/src/app/button/button.component.ts
@@ -0,0 +1,33 @@
+import {
+  Input,
+  Component,
+  ViewEncapsulation,
+  EventEmitter,
+  Output
+} from '@angular/core';
+
+@Component({
+  selector: 'custom-button',
+  template: `<h1>pb-2</h1><button (click)="handleClick()">{{label}}</button>`,
+  styles: [
+    `
+    button {
+      border: solid 3px;
+      padding: 8px 10px;
+      background: #bada55;
+      font-size: 20px;
+    }
+  `
+  ],
+  encapsulation: ViewEncapsulation.Native
+})
+export class ButtonComponent {
+  @Input() label = 'default label';
+  @Output() action = new EventEmitter<number>();
+  private clicksCt = 0;
+
+  handleClick() {
+    this.clicksCt++;
+    this.action.emit(this.clicksCt);
+  }
+}
