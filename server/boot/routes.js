37057cb355b535567e0b973d86267634bfabbc00
Lane Campbell <ldc0618@gmail.com>
2019-06-04 15:44:12 -0700

    add fa to pb

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -59,7 +59,7 @@ module.exports = function (app) {
 
   app.get('*', async function(req, res, next) {
     if (req.url === '/' || req.url === '/explorer' || req.url.startsWith('/api')
-    || req.url.endsWith('.css') || req.url.endsWith('.js')) {
+    || req.url.endsWith('.css') || req.url.endsWith('.js') || req.url.indexOf('fonts') > -1) {
       // log(chalk.blue(req.url));
       return next();
     }
