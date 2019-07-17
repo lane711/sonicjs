acca6de730b4c48a0a89b5946a2925122600585f
Lane Campbell <ldc0618@gmail.com>
2019-07-16 20:16:00 -0700

    handlebar dynamc templating

--- server/services/list.service.js
+++ server/services/list.service.js
@@ -7,17 +7,30 @@ const ShortcodeTree = require('shortcode-tree').ShortcodeTree;
 const chalk = require('chalk');
 const log = console.log;
 
-const Formio = {};
-const document = { getElementById: {} };
+var handlebars = require('handlebars');
+
 
 module.exports = {
 
     getList: async function (contentType) {
+
+        // var hbs = exphbs.create({ /* config */ });
+
+        var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
+             "{{kids.length}} kids:</p>" +
+             "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+var template = handlebars.compile(source);
+ 
+var data = { "name": "Alan", "hometown": "Somewhere, TX",
+             "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+var result = template(data);
+
+
         let list = "list goes here";
         // let fieldsDef = await this.getFormDefinition(contentType);
         // form +=  JSON.stringify(fieldsDef);
         // form += "</script>";
         // form += await this.getFormTemplate();
-        return list;
+        return result;
     }
 }
