9d7361c8e1d53ff9c8ca4f1859157a4d0f377d88
Lane Campbell <ldc0618@gmail.com>
2019-10-09 18:07:45 -0700

    add pikr

--- server/page-builder/js/page-builder.js
+++ server/page-builder/js/page-builder.js
@@ -204,7 +204,50 @@ async function showBackgroundTypeOptions(backgroundSetting, sectionId) {
 }
 
 async function setupColorPicker(currentSectionId) {
-    debugger;
+
+    const pickr = Pickr.create({
+        el: `#backgroundColorPreview-${currentSectionId}`,
+        theme: 'nano', // or 'monolith', or 'nano'
+    
+        swatches: [
+            'rgba(244, 67, 54, 1)',
+            'rgba(233, 30, 99, 0.95)',
+            'rgba(156, 39, 176, 0.9)',
+            'rgba(103, 58, 183, 0.85)',
+            'rgba(63, 81, 181, 0.8)',
+            'rgba(33, 150, 243, 0.75)',
+            'rgba(3, 169, 244, 0.7)',
+            'rgba(0, 188, 212, 0.7)',
+            'rgba(0, 150, 136, 0.75)',
+            'rgba(76, 175, 80, 0.8)',
+            'rgba(139, 195, 74, 0.85)',
+            'rgba(205, 220, 57, 0.9)',
+            'rgba(255, 235, 59, 0.95)',
+            'rgba(255, 193, 7, 1)'
+        ],
+    
+        components: {
+    
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+    
+            // Input / output Options
+            interaction: {
+                hex: false,
+                rgba: false,
+                hsla: false,
+                hsva: false,
+                cmyk: false,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+
+
     var parent = document.querySelector(`#backgroundColorPreview-${currentSectionId}`);
     // var parent = $('#background-color-preview');
 
