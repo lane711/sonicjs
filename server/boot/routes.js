1f5549cf2aa36f1c676dff7a2493c2d3a7c6293a
Lane Campbell <ldc0618@gmail.com>
2019-10-17 12:02:16 -0700

    remove helmet

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -16,14 +16,12 @@ var cssService = require('../services/css.service');
 cssService.startup();
 var userService = require('../services/user.service');
 
-
 var cors = require('cors');
 
 const chalk = require('chalk');
 const log = console.log;
 
 var admin = require(__dirname + '/admin');
-const helmet = require('helmet')
 
 module.exports = function (app) {
   var router = app.loopback.Router();
@@ -210,35 +208,6 @@ module.exports = function (app) {
     }
 
   });
-  // app.use('/admin', function(req, res, next) {
-  //   console.log('admin route', req.url);
-  //   res.send(adminPage);
-  // });
-
-  // app.use(/\/((?!admin).)*/, function(){
-  //   return "ok";
-  // });
-
-  // app.get('*', function(req, res){
-  //   res.send(adminPage);
-  // });
-
-  // Allow from a specific host.
-  // Sets "X-Frame-Options: ALLOW-FROM http://example.com".
-  // app.use(helmet.frameguard({
-  //   action: 'allow-from',
-  //   domain: 'http://localhost:4200'
-  // }))
-
-  // app.use(helmet.frameguard({ action: 'sameorigin' }))
-
-
-  // app.use(helmet.frameguard({ action: 'allow' }))
-
-
-  // app.use(helmet.frameguard({ action: 'sameorigin' }))
-
-
 
   app.use(router);
 
