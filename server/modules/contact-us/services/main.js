e03709f151a0d7f03c50d3d8dcf53bf99490410d
Lane Campbell <ldc0618@gmail.com>
2019-11-04 19:34:10 -0800

    setup send grid

--- server/modules/contact-us/services/main.js
+++ server/modules/contact-us/services/main.js
@@ -10,8 +10,31 @@ module.exports = contactUsMainService = {
     startup: async function () {
         eventBusService.on('beforeSave', async function (options) {
 
-            if(options.data.contenType === 'contact-us'){
-                
+            if (options.instance.contentType === 'contact') {
+
+                var helper = require('sendgrid').mail;
+                var from_email = new helper.Email('ldc0618@gmail.com');
+                var to_email = new helper.Email('ldc0618@gmail.com');
+                var subject = 'Hello World from the SendGrid Node.js Library!';
+                var content = new helper.Content('text/plain', 'Hello, Email!');
+                var mail = new helper.Mail(from_email, subject, to_email, content);
+
+                var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+                var request = sg.emptyRequest({
+                    method: 'POST',
+                    path: '/v3/mail/send',
+                    body: mail.toJSON(),
+                });
+
+                sg.API(request, function (error, response) {
+                    if(error){
+                        console.log(error);
+                    }
+                    console.log(response.statusCode);
+                    console.log(response.body);
+                    console.log(response.headers);
+                });
+
             }
             // if (options.shortcode.name === 'MODULE-HELLO-WORLD') {
             //     let id = options.shortcode.properties.id;
