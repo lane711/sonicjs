64677017b6b2c1f72cba30aee430db437ba0dcf9
Lane Campbell <ldc0618@gmail.com>
2019-11-19 20:25:24 -0800

    fix form service load in browser

--- server/services/data.service.js
+++ server/services/data.service.js
@@ -1,4 +1,5 @@
-if (require) {
+//check if running in node (and not the browser)
+if (typeof module !== 'undefined' && module.exports) {
     var eventBusService = require('./event-bus.service');
     var globalService = require('./global.service');
     var pageBuilderService = require('./page-builder.service');
--- server/services/form.service.js
+++ server/services/form.service.js
@@ -1,19 +1,21 @@
-var dataService = require('./data.service');
-var eventBusService = require('./event-bus.service');
+if (typeof module !== 'undefined' && module.exports) {
+    var dataService = require('./data.service');
+    var eventBusService = require('./event-bus.service');
 
-var fs = require('fs');
-const cheerio = require('cheerio')
-const axios = require('axios');
-const ShortcodeTree = require('shortcode-tree').ShortcodeTree;
-const chalk = require('chalk');
-const log = console.log;
+    var fs = require('fs');
+    const cheerio = require('cheerio')
+    const axios = require('axios');
+    const ShortcodeTree = require('shortcode-tree').ShortcodeTree;
+    const chalk = require('chalk');
+    const log = console.log;
 
-const Formio = {};
-const document = { getElementById: {} };
+    const Formio = {};
+    const document = { getElementById: {} };
+}
 
-(function(exports){
+(function (exports) {
 
-// module.exports = formService = {
+    // module.exports = formService = {
 
     // startup: function () {
     //     console.log('>>=== form service startup');
@@ -31,128 +33,128 @@ const document = { getElementById: {} };
         });
     },
 
-    exports.getForm = async function (contentTypeId, content) {
-        let contentType;
-        if (content) {
-            contentType = await dataService.getContentType(content.data.contentType);
-        } else if (contentTypeId) {
-            contentType = await dataService.getContentType(contentTypeId);
-        }
+        exports.getForm = async function (contentTypeId, content) {
+            let contentType;
+            if (content) {
+                contentType = await dataService.getContentType(content.data.contentType);
+            } else if (contentTypeId) {
+                contentType = await dataService.getContentType(contentTypeId);
+            }
 
-        let name = `${contentType.systemid}Form`;
-        let settings = await this.getFormSettings(contentType, content);
-        let components = await this.getFormComponents(contentType, content);
-        const formJSON = {
-            "components": components,
-            "name": name,
-            "settings": settings
-        }
+            let name = `${contentType.systemid}Form`;
+            let settings = await this.getFormSettings(contentType, content);
+            let components = await this.getFormComponents(contentType, content);
+            const formJSON = {
+                "components": components,
+                "name": name,
+                "settings": settings
+            }
 
-        let form = "<script type='text/javascript'> const formJSON = ";
-        form += JSON.stringify(formJSON);
-        form += "</script>";
-
-
-        form += "<script type='text/javascript'> const formValuesToLoad = ";
-        if (content) {
-            form += JSON.stringify(content.data);
-        }
-        else {
-            form += "{}";
-        }
-        form += "</script>";
+            let form = "<script type='text/javascript'> const formJSON = ";
+            form += JSON.stringify(formJSON);
+            form += "</script>";
 
 
-        form += await this.getFormTemplate();
-        return form;
-    },
+            form += "<script type='text/javascript'> const formValuesToLoad = ";
+            if (content) {
+                form += JSON.stringify(content.data);
+            }
+            else {
+                form += "{}";
+            }
+            form += "</script>";
 
-    exports.getFormTemplate = async function () {
-        let themePath = __dirname + '/../assets/html/form.html';
 
-        return new Promise((resolve, reject) => {
-            fs.readFile(themePath, "utf8", (err, data) => {
-                if (err) {
-                    console.log(err);
-                    reject(err);
-                }
-                else {
-                    resolve(data);
-                }
+            form += await this.getFormTemplate();
+            return form;
+        },
+
+        exports.getFormTemplate = async function () {
+            let themePath = __dirname + '/../assets/html/form.html';
+
+            return new Promise((resolve, reject) => {
+                fs.readFile(themePath, "utf8", (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                    }
+                    else {
+                        resolve(data);
+                    }
+                });
             });
-        });
-    },
+        },
 
-    exports.getFormSettings = async function (contentType, content) {
-        let settings = {};
-        settings.recaptcha = {
-            "isEnabled": "true", 
-            "siteKey": process.env.RECAPTCHA_SITE_KEY
-        }
+        exports.getFormSettings = async function (contentType, content) {
+            let settings = {};
+            settings.recaptcha = {
+                "isEnabled": "true",
+                "siteKey": process.env.RECAPTCHA_SITE_KEY
+            }
             return settings;
-    },
+        },
 
-    exports.getFormComponents = async function (contentType, content) {
+        exports.getFormComponents = async function (contentType, content) {
 
-        // let contentTypeDef = await dataService.getContentType(content.data.contentType);
-        // console.log('contentTypeDef', contentTypeDef);
-        let components = contentType.components;
+            // let contentTypeDef = await dataService.getContentType(content.data.contentType);
+            // console.log('contentTypeDef', contentTypeDef);
+            let components = contentType.components;
 
-        if (content) {
-            this.addBaseContentTypeFields(content.id, content.data.contentType, components);
-        } else{
-            components.push({
+            if (content) {
+                this.addBaseContentTypeFields(content.id, content.data.contentType, components);
+            } else {
+                components.push({
+                    type: 'textfield',
+                    key: 'contentType',
+                    label: 'contentType',
+                    defaultValue: contentType.systemid,
+                    hidden: true,
+                    input: true
+                });
+            }
+
+            return components;
+        },
+
+        exports.addBaseContentTypeFields = function (id, contentType, controls) {
+            // console.log('addBaseContentTypeFields', contentType, controls);
+
+            controls.push({
                 type: 'textfield',
-                key: 'contentType',
-                label: 'contentType',
-                defaultValue: contentType.systemid,
+                key: 'id',
+                label: 'id',
+                defaultValue: id,
                 hidden: true,
                 input: true
             });
+
+            //   controls.push({
+            //     type: 'textfield',
+            //     key: 'contentTypeId',
+            //     label: 'contentTypeId',
+            //     defaultValue: contentType,
+            //     input: true
+            //   });
+
+            // if(isToBePopulatedWithExistingContent){
+            //   let controlId = new HiddenQuestion({
+            //     key: 'id',
+            //     label: 'Id',
+            //     value: contentType.id,
+            //     required: true,
+            //     order: 0
+            //   });
+            //   controls.push(controlId);
+
+            //   let controlContentType = new HiddenQuestion({
+            //     key: 'contentTypeId',
+            //     label: 'Content Type',
+            //     value: contentType.systemid,
+            //     required: true,
+            //     order: 1
+            //   });
+            //   controls.push(controlContentType);
+            // }
         }
-
-        return components;
-    },
-
-    exports.addBaseContentTypeFields = function (id, contentType, controls) {
-        // console.log('addBaseContentTypeFields', contentType, controls);
-
-        controls.push({
-            type: 'textfield',
-            key: 'id',
-            label: 'id',
-            defaultValue: id,
-            hidden: true,
-            input: true
-        });
-
-        //   controls.push({
-        //     type: 'textfield',
-        //     key: 'contentTypeId',
-        //     label: 'contentTypeId',
-        //     defaultValue: contentType,
-        //     input: true
-        //   });
-
-        // if(isToBePopulatedWithExistingContent){
-        //   let controlId = new HiddenQuestion({
-        //     key: 'id',
-        //     label: 'Id',
-        //     value: contentType.id,
-        //     required: true,
-        //     order: 0
-        //   });
-        //   controls.push(controlId);
-
-        //   let controlContentType = new HiddenQuestion({
-        //     key: 'contentTypeId',
-        //     label: 'Content Type',
-        //     value: contentType.systemid,
-        //     required: true,
-        //     order: 1
-        //   });
-        //   controls.push(controlContentType);
-        // }
-    }
-// }
-})(typeof exports === 'undefined'? this['formService']={}: exports);
+    // }
+})(typeof exports === 'undefined' ? this['formService'] = {} : exports);
