d3345cb11d534457826ee79974d90d47781b8423
Lane Campbell <ldc0618@gmail.com>
2019-12-20 19:04:48 -0800

    add text module

--- server/modules/text/services/text-main-service.js
+++ server/modules/text/services/text-main-service.js
@@ -1,18 +1,18 @@
 var dataService = require('../../../services/data.service');
 var eventBusService = require('../../../services/event-bus.service');
 var globalService = require('../../../services/global.service');
+var contentService = require('../../../services/content.service');
 
-module.exports = htmlMainService = {
+module.exports = textMainService = {
 
     startup: async function () {
         eventBusService.on('beginProcessModuleShortCode', async function (options) {
 
-                if (options.shortcode.name === 'HTML') {
+            if (options.shortcode.name === 'TEXT') {
 
-                    options.moduleName = 'feature-box';
-                    await moduleService.processModuleInColumn(options);
-                }
-
+                options.moduleName = 'feature-box';
+                await moduleService.processModuleInColumn(options);
+            }
         });
     },
 
--- server/views/js_templates/module-default-main-service.js
+++ server/views/js_templates/module-default-main-service.js
@@ -8,22 +8,12 @@ module.exports = {{ systemidCamelCase }}MainService = {
         eventBusService.on('beginProcessModuleShortCode', async function (options) {
 
             if (options.shortcode.name === '{{ systemidUpperCase }}') {
-                let id = options.shortcode.properties.id;
-                let contentType = options.shortcode.properties.contentType;
-                let viewPath = __dirname + `/../views/{{ systemid }}-main.handlebars`;
-                let viewModel = await dataService.getContentById(id);
-                let proccessedHtml = await helloWorldMainService.processView(contentType, viewModel, viewPath);
 
-                globalService.pageContent = globalService.pageContent.replace(options.shortcode.codeText, proccessedHtml);
+                options.moduleName = '{{ systemid }}';
+                await moduleService.processModuleInColumn(options);
             }
 
         });
     },
-
-    processView: async function (contentType, viewModel, viewPath) {
-        var result = await viewService.getProccessedView(contentType, viewModel, viewPath);
-
-        return result;
-    }
 
 }
