6ee629e7dfca04e7bd78f2f3dba1c4a9fc7c7dec
Lane Campbell <ldc0618@gmail.com>
2020-01-06 19:26:50 -0800

    add headerjs token

--- server/modules/google-analytics/services/google-analytics-main-service.js
+++ server/modules/google-analytics/services/google-analytics-main-service.js
@@ -5,15 +5,34 @@ var globalService = require('../../../services/global.service');
 module.exports = googleAnalyticsMainService = {
 
     startup: async function () {
-        eventBusService.on('beginProcessModuleShortCode', async function (options) {
+        // eventBusService.on('beginProcessModuleShortCode', async function (options) {
 
-            if (options.shortcode.name === 'GOOGLE-ANALYTICS') {
+        //     if (options.shortcode.name === 'GOOGLE-ANALYTICS') {
 
-                options.moduleName = 'google-analytics';
-                await moduleService.processModuleInColumn(options);
+        //         options.moduleName = 'google-analytics';
+        //         await moduleService.processModuleInColumn(options);
+        //     }
+
+        // });
+
+        eventBusService.on('getRenderedPagePostDataFetch', async function (options) {
+            if (options && options.page) {
+                await googleAnalyticsMainService.addHeaderJs(options);
             }
-
         });
+    },
+
+    addHeaderJs: async function (options) {
+        let googleAnalyticUSCode = '12344';
+        options.page.data.headerJs = `  <script async="" src="https://www.googletagmanager.com/gtag/js?id=${googleAnalyticUSCode}"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date());
+      
+          gtag('config', '${googleAnalyticUSCode}');
+        </script>`;
+
     },
 
 }
