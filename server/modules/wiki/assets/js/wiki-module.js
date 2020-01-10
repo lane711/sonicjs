be2d7cf21d4fdcb7832ed90c4682f51049a34423
Lane Campbell <ldc0618@gmail.com>
2020-01-09 18:25:12 -0800

    setup wiki ace

--- server/modules/wiki/assets/js/wiki-module.js
+++ server/modules/wiki/assets/js/wiki-module.js
@@ -1,1 +1,12 @@
 // JS File for Module: wiki
+$(document).ready(async function () {
+    setupACEEditor1();
+});
+async function setupACEEditor1() {
+    if ($('#editor1').length === 0) {
+        return;
+    }
+    var editor1 = ace.edit("editor1");
+    editor1.setTheme("ace/theme/monokai");
+    editor1.session.setMode("ace/mode/css");
+}
