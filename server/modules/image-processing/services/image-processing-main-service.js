356e9f9eac98a2e438196f8e79cd1f7f6330d0a0
Lane Campbell <ldc0618@gmail.com>
2020-02-18 09:35:22 -0800

    image processing by height

--- server/modules/image-processing/services/image-processing-main-service.js
+++ server/modules/image-processing/services/image-processing-main-service.js
@@ -3,7 +3,7 @@ var eventBusService = require("../../../services/event-bus.service");
 var globalService = require("../../../services/global.service");
 var jimp = require("jimp");
 var path = require("path");
-const fs = require('fs')
+const fs = require("fs");
 
 module.exports = imageProcessingMainService = {
   startup: async function() {
@@ -16,37 +16,65 @@ module.exports = imageProcessingMainService = {
 
     eventBusService.on("requestBegin", async function(options) {
       if (options.req.url.startsWith("/images/")) {
+        let filePath = decodeURIComponent(options.req.path);
+
+        if (!isSupportedImageType(filePath)) {
+          return;
+        }
 
         options.req.isRequestAlreadyHandled = true;
 
         // Read the image.
-        let filePath = decodeURIComponent(options.req.path);
-        let fileName = filePath.replace('/images/','');
+        let fileName = filePath.replace("/images/", "");
         let width = options.req.query.width;
+        let height = options.req.query.height;
 
-        let imagePath = path.join(__dirname, "../../..", `/storage/files/${fileName}`);
+        let imagePath = path.join(
+          __dirname,
+          "../../..",
+          `/storage/files/${fileName}`
+        );
         let newImagePath = `server/storage/files/width-${width}/${fileName}`;
 
-        if(!width){
+        let widthBasedResize = true;
+        if (!width && height) {
+          newImagePath = `server/storage/files/height-${height}/${fileName}`;
+          widthBasedResize = false;
+        }
+
+        if (!width && !height) {
           let originalImagePath = `server/storage/files/${fileName}`;
-          options.res.sendFile(originalImagePath, {root: './'})
+          options.res.sendFile(originalImagePath, { root: "./" });
         }
 
         try {
           if (!fs.existsSync(newImagePath)) {
             //file does not exist
-            console.log(`creating ${fileName} at width: ${width}`);
+            console.log(
+              `creating ${fileName} with width: ${width}, height: ${height} at: ${newImagePath}`
+            );
             const image = await jimp.read(imagePath);
             image.quality(100);
-            await image.resize(parseInt(width), jimp.AUTO);
+            if (widthBasedResize) {
+              await image.resize(parseInt(width), jimp.AUTO);
+            } else {
+              await image.resize(jimp.AUTO, parseInt(height));
+            }
             let img = await image.writeAsync(newImagePath);
           }
-        } catch(err) {
-          console.error(err)
+        } catch (err) {
+          console.error(err);
         }
 
-        options.res.sendFile(newImagePath, {root: './'})
+        options.res.sendFile(newImagePath, { root: "./" });
       }
     });
+
+    function isSupportedImageType(filePath) {
+      let extension = filePath.split('.').pop();
+      var supportedImageTypes = ["jpg", "jpeg", "png", "gif", "bmp"];
+      var isSupportedImageType = supportedImageTypes.includes(extension, 0);
+      return isSupportedImageType;
+    }
   }
 };
