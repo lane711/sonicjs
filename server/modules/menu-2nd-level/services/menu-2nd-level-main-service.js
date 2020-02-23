e99a1fae92432f3d5a07b8ad65f569ecf38331f8
Lane Campbell <ldc0618@gmail.com>
2020-02-23 08:51:16 -0800

    add helper

--- server/modules/menu-2nd-level/services/menu-2nd-level-main-service.js
+++ server/modules/menu-2nd-level/services/menu-2nd-level-main-service.js
@@ -8,22 +8,34 @@ const path = require("path");
 module.exports = menu2ndLevelMainService = {
   startup: async function() {
     //register hbs helper
-    await Handlebars.registerHelper("postMenu", async function() {
-      let viewModel = [
-        { title: "title", link: "/whatever" },
-        { title: "title2", link: "/whatever2" }
-      ];
-      let viewPath = path.join(
-        __dirname,
-        "..",
-        "views/menu-2nd-level-main.handlebars"
-      ); //  __dirname + `/../views/menu-2nd-level-main.handlebars`;
-      console.log("viewPath", viewPath);
-      var result = await viewService.getProccessedView("", viewModel, viewPath);
+
+    let viewModel = [
+      { title: "title", link: "/whatever" },
+      { title: "title2", link: "/whatever2" }
+    ];
+    let viewPath = path.join(
+      __dirname,
+      "..",
+      "views/menu-2nd-level-main.handlebars"
+    ); //  __dirname + `/../views/menu-2nd-level-main.handlebars`;
+    console.log("viewPath", viewPath);
+
+    var result = await viewService.getProccessedView("", viewModel, viewPath);
+
+    Handlebars.registerHelper("postMenuHelper", function() {
+
 
       console.log("result", result);
 
       return result;
+    });
+
+    Handlebars.registerHelper("menuHelper", function(originalMenu) {
+
+
+      console.log("result", result);
+
+      return originalMenu;
     });
 
     eventBusService.on("beginProcessModuleShortCode", async function(options) {
