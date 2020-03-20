6d8319bca7bcfaeaed9875c66cef30754eeca705
Lane Campbell <ldc0618@gmail.com>
2020-03-19 21:08:43 -0700

    matrix update

--- server/modules/compare/services/compare-main-service.js
+++ server/modules/compare/services/compare-main-service.js
@@ -21,24 +21,40 @@ module.exports = compareMainService = {
 
       let contentType = await dataService.getContentType("compare-item");
       let tabs = contentType.components[0].components;
+      options.viewModel.data.contentType = tabs;
 
       let matrix = compareMainService.getMatrixData(tabs, compareItems);
-
-      options.viewModel.data.contentType = tabs;
+      options.viewModel.data.matrix = matrix;
     });
   },
 
   getMatrixData: function(contentType, compareItems) {
+    let rows = [];
     contentType.forEach(group => {
-      console.log(group.label);
+      let row = { columns: [] };
+      row.columns.push(group.label);
+      // console.log(group.label);
       group.components.forEach(element => {
-        console.log("->" + element.label);
+        // console.log("->" + element.label);
 
-        //find matching compare item row
-        compareItems.forEach(complareItem => {
-          console.log(complareItem.data.productName);
-        });
+        if (element.components) {
+          let row = { columns: [] };
+          element.components.forEach(subElement => {
+            row.columns.push(subElement.label);
+
+            //find matching compare item row
+            compareItems.forEach(complareItem => {
+              let column = complareItem.data[element.key];
+              row.columns.push(column);
+            });
+          });
+          rows.push(row);
+
+        }
+
       });
+      rows.push(row);
     });
+    return rows;
   }
 };
