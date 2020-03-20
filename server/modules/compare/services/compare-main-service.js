a33f06c09baf375953010bc02b5d63af656ff685
Lane Campbell <ldc0618@gmail.com>
2020-03-19 20:15:48 -0700

    matrix data stub

--- server/modules/compare/services/compare-main-service.js
+++ server/modules/compare/services/compare-main-service.js
@@ -1,31 +1,44 @@
-var dataService = require('../../../services/data.service');
-var eventBusService = require('../../../services/event-bus.service');
-var globalService = require('../../../services/global.service');
+var dataService = require("../../../services/data.service");
+var eventBusService = require("../../../services/event-bus.service");
+var globalService = require("../../../services/global.service");
 
 module.exports = compareMainService = {
+  startup: async function() {
+    eventBusService.on("beginProcessModuleShortCode", async function(options) {
+      if (options.shortcode.name === "COMPARE") {
+        options.moduleName = "compare";
+        await moduleService.processModuleInColumn(options);
+      }
+    });
 
-    startup: async function () {
-        eventBusService.on('beginProcessModuleShortCode', async function (options) {
+    eventBusService.on("postModuleGetData", async function(options) {
+      if (options.shortcode.name !== "COMPARE") {
+        return;
+      }
 
-            if (options.shortcode.name === 'COMPARE') {
+      let compareItems = await dataService.getContentByType("compare-item");
+      options.viewModel.data.compareItems = compareItems;
 
-                options.moduleName = 'compare';
-                await moduleService.processModuleInColumn(options);
-            }
+      let contentType = await dataService.getContentType("compare-item");
+      let tabs = contentType.components[0].components;
 
+      let matrix = compareMainService.getMatrixData(tabs, compareItems);
+
+      options.viewModel.data.contentType = tabs;
+    });
+  },
+
+  getMatrixData: function(contentType, compareItems) {
+    contentType.forEach(group => {
+      console.log(group.label);
+      group.components.forEach(element => {
+        console.log("->" + element.label);
+
+        //find matching compare item row
+        compareItems.forEach(complareItem => {
+          console.log(complareItem.data.productName);
         });
-
-        eventBusService.on('postModuleGetData', async function (options) {
-
-          if (options.shortcode.name !== 'COMPARE') {
-              return;
-          }
-
-          let width = options.viewModel.data.thumbnailWidth;
-          options.viewModel.data.thumbnailWidthRetina = width *2;
-
       });
-
-    },
-
-}
+    });
+  }
+};
