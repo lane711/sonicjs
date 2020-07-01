7e03292fa2f82c313c8e311248efa62b757c112c
Lane Campbell <ldc0618@gmail.com>
2020-07-01 07:36:13 -0700

    readme update

--- README.md
+++ README.md
@@ -186,7 +186,7 @@ Presently, the only in-production implementations that I know about (aside from 
 
 ## Versioning
 
-We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/lane711/sonicjs/tags).
 
 ## Authors
 
--- server/modules/contact-us/services/contact-us-main-service.js
+++ server/modules/contact-us/services/contact-us-main-service.js
@@ -1,60 +1,63 @@
-var dataService = require('../../../services/data.service');
-var eventBusService = require('../../../services/event-bus.service');
-var globalService = require('../../../services/global.service');
-var formService = require('../../../services/form.service');
-var dataService = require('../../../services/data.service');
-var emailService = require('../../../services/email.service')
+var dataService = require("../../../services/data.service");
+var eventBusService = require("../../../services/event-bus.service");
+var globalService = require("../../../services/global.service");
+var formService = require("../../../services/form.service");
+var dataService = require("../../../services/data.service");
+var emailService = require("../../../services/email.service");
 
 module.exports = contactUsMainService = {
+  startup: async function () {
+    eventBusService.on("beginProcessModuleShortCode", async function (options) {
+      if (options.shortcode.name !== "CONTACT-US") {
+        return;
+      }
 
-    startup: async function () {
-        eventBusService.on('beginProcessModuleShortCode', async function (options) {
+      options.moduleName = "contact-us";
+      await moduleService.processModuleInColumn(options);
+    });
 
-            if (options.shortcode.name !== 'CONTACT-US') {
-                return;
-            }
+    eventBusService.on("postModuleGetData", async function (options) {
+      if (options.shortcode.name !== "CONTACT-US") {
+        return;
+      }
 
-            options.moduleName = 'contact-us';
-            await moduleService.processModuleInColumn(options);
+      options.viewModel.data.form = await formService.getForm(
+        "contact",
+        undefined,
+        "submitForm(submission)"
+      );
 
+      // console.log('contact module after view model', options.viewModel);
+    });
 
-        });
+    eventBusService.on("afterFormSubmit", async function (options) {
+      if (options.data.contentType !== "contact") {
+        return;
+      }
 
-        eventBusService.on('postModuleGetData', async function (options) {
+      // save the form
+      await dataService.createContentInstance(options);
 
-            if (options.shortcode.name !== 'CONTACT-US') {
-                return;
-            }
+      // send the emails
+      let contact = options.data;
 
-            options.viewModel.data.form = await formService.getForm('contact', undefined, "submitForm(submission)");
+      //confirmation to user
+      let body = `Hi ${contact.name}, \n\nThanks for reaching out. We'll get back to you ASAP.\n\nFor your reference, here was your message:\n${contact.message}`;
+      await emailService.sendEmail(
+        contact.email,
+        "lane@sonicjs.com",
+        "SoncisJs Message Received",
+        body
+      );
 
-            // console.log('contact module after view model', options.viewModel);
-
-        });
-
-        eventBusService.on('afterFormSubmit', async function (options) {
-
-            if (options.data.contentType !== 'contact') {
-                return;
-            }
-
-            // save the form
-            await dataService.createContentInstance(options);
-
-            // send the emails
-            let contact = options.data;
-
-            //confirmation to user
-            let body = `Hi ${contact.name}, \n\nThanks for reaching out. We'll get back to you ASAP.\n\nFor your reference, here was your message:\n${contact.message}`
-            await emailService.sendEmail(contact.email, 'admin@sonicjs.com', 'SoncisJs Message Received', body);
-
-
-            //admin notification
-            let adminBody = `${contact.name} (${contact.email}) wrote: \n\n${contact.message}`
-            await emailService.sendEmail('admin@sonicjs.com', contact.email, 'SoncisJs Contact', adminBody);
-
-        });
-
-    },
-
-}
+      //admin notification
+      let adminBody = `${contact.name} (${contact.email}) wrote: \n\n${contact.message}`;
+      await emailService.sendEmail(
+        "lane@sonicjs.com",
+        contact.email,
+        "SoncisJs Contact",
+        adminBody
+      );
+    });
+  },
+};
