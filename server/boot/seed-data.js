5c2173afc708813672a8d4dd955f46402e134ccd
Lane Campbell <ldc0618@gmail.com>
2020-07-30 19:53:35 -0700

    remove old content types

--- server/boot/seed-data.js
+++ server/boot/seed-data.js
@@ -1,26 +1,21 @@
+const fs = require("fs");
+
 module.exports = function (app) {
+  return;
   // app.dataSources.primary.automigrate();
-  // app.dataSources.mongo.automigrate("contentType", function (err) {
-  //   if (err) throw err;
-  //   app.models.contentType.create(
-  //     [
-  //       {
-  //         systemid: "news6",
-  //         title: "News Article 6",
-  //         submit: "true",
-  //         testAttr: "ipsum de lor 6",
-  //       },
-  //       {
-  //         systemid: "news9",
-  //         title: "News Article 9",
-  //         submit: "true",
-  //         testAttr: "ipsum de lor 9",
-  //       },
-  //     ],
-  //     function (err, coffeeShops) {
-  //       if (err) throw err;
-  //       console.log("Models created: \n", coffeeShops);
-  //     }
-  //   );
-  // });
+  let dataRaw = fs.readFileSync("server/data/data.json");
+  let data = JSON.parse(dataRaw);
+
+  app.dataSources.primary.automigrate("contentType", function (err) {
+    if (err) throw err;
+
+    let contentType = data.models.contentType;
+    // app.models.contentType.create([data.models.contentType], function (
+    //   err,
+    //   coffeeShops
+    // ) {
+    //   if (err) throw err;
+    //   console.log("Models created: \n", coffeeShops);
+    // });
+  });
 };
