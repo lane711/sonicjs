9f2019d361ed130606399aae309d1be1ce0e1020
Lane Campbell <ldc0618@gmail.com>
2020-07-31 21:00:10 -0700

    soft code site logo

--- server/services/formatting.service.js
+++ server/services/formatting.service.js
@@ -1,32 +1,30 @@
 var moment = require("moment");
 
 module.exports = formattingService = {
-  formatDates: async function(data, dateOnly = false) {
-    let dateFormat = dateOnly
-      ? "YYYY-MM-DD"
-      : "YYYY-MM-DD, h:mm:ss a";
+  formatDates: async function (data, dateOnly = false) {
+    let dateFormat = dateOnly ? "YYYY-MM-DD" : "YYYY-MM-DD, h:mm:ss a";
 
-    data.forEach(element => {
+    data.forEach((element) => {
       if (element.data.createdOn) {
         var day = moment(element.data.createdOn);
-        element.data.createdOnFormatted = moment(day).format(
-          dateFormat
-        );
+        element.data.createdOnFormatted = moment(day).format(dateFormat);
       } else {
         element.data.createdOnFormatted = "1900-01-01, 12:00:00 am";
       }
     });
   },
 
-  formatTitles: async function(data) {
+  formatTitles: async function (data) {
     let length = 50;
-    data.forEach(element => {
+    data.forEach((element) => {
       if (!element.data.title) {
         let sourceCopy = "";
         if (element.data.body) {
           sourceCopy = element.data.body;
         } else if (element.data.text) {
           sourceCopy = element.data.text;
+        } else if (element.data.contentType) {
+          sourceCopy = element.data.contentType;
         }
         let rawBody = this.stripHtmlTags(sourceCopy);
         if (rawBody) {
@@ -39,9 +37,9 @@ module.exports = formattingService = {
     });
   },
 
-  stripHtmlTags: function(str) {
+  stripHtmlTags: function (str) {
     if (str === null || str === "") return false;
     else str = str.toString();
     return str.replace(/<[^>]*>/g, "");
-  }
+  },
 };
