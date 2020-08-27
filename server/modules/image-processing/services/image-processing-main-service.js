4cadab9a96c352de8a41a81c5bf4f39a9eb14204
Lane Campbell <ldc0618@gmail.com>
2020-08-26 17:57:58 -0700

    cleanup images

--- server/modules/image-processing/services/image-processing-main-service.js
+++ server/modules/image-processing/services/image-processing-main-service.js
@@ -71,6 +71,7 @@ module.exports = imageProcessingMainService = {
             } else {
               await image.resize(jimp.AUTO, parseInt(height));
             }
+            console.log("attempting write:" + newImagePath);
             let img = await image.writeAsync(newImagePath);
           }
         } catch (err) {
