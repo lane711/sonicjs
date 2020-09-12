2f8145592ec26c08799ee076f4b1916f47ad7af1
Lane Campbell <ldc0618@gmail.com>
2020-09-11 19:38:04 -0700

    add and sub roles from user

--- server/models/user.js
+++ server/models/user.js
@@ -2,24 +2,27 @@
 // Node module: loopback-example-user-management
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-'use strict';
+"use strict";
 
-var config = require('../../server/config.json');
-var path = require('path');
+var config = require("../../server/config.json");
+var path = require("path");
 var loopback = require("loopback");
+const { forEach } = require("lodash");
 
 //Replace this address with your actual address
-var senderAddress = 'test@test.com';
+var senderAddress = "test@test.com";
 
 module.exports = function (User) {
-
-  User.afterRemote('login', function(ctx){
-    ctx.res.cookie('access_token', ctx.result.id, { signed: true, maxAge: ctx.result.ttl * 1000 });
+  User.afterRemote("login", function (ctx) {
+    ctx.res.cookie("access_token", ctx.result.id, {
+      signed: true,
+      maxAge: ctx.result.ttl * 1000,
+    });
     return Promise.resolve();
   });
 
-  User.afterRemote('logout', function(ctx){
-    ctx.res.clearCookie('access_token');
+  User.afterRemote("logout", function (ctx) {
+    ctx.res.clearCookie("access_token");
     return Promise.resolve();
   });
 
@@ -29,15 +32,15 @@ module.exports = function (User) {
   // });
 
   //send verification email after registration
-  User.afterRemote('create', function (context, user, next) {
+  User.afterRemote("create", function (context, user, next) {
     var options = {
-      type: 'email',
+      type: "email",
       to: user.email,
       from: senderAddress,
-      subject: 'Thanks for registering.',
-      template: path.resolve(__dirname, '../../server/views/verify.ejs'),
-      redirect: '/verified',
-      user: user
+      subject: "Thanks for registering.",
+      template: path.resolve(__dirname, "../../server/views/verify.ejs"),
+      redirect: "/verified",
+      user: user,
     };
 
     // user.verify(options, function (err, response) {
@@ -55,78 +58,107 @@ module.exports = function (User) {
     // });
   });
 
-  User.afterRemote('*', function (context, user, next) {
-
+  User.afterRemote("*", function (context, user, next) {
     //map roles
-if(context.method.name === 'replaceById'){
-  var roleMappingModel = loopback.getModel("RoleMapping");
-  roleMappingModel.upsertWithWhere(
-    {
-      "principalType": "user",
-      "principalId": 1,
-      "roleId": 1
-    },{
-      "principalType": "user",
-      "principalId": 1,
-      "roleId": 1
-    },
-    function (err, rmInstance) {
-      console.log(rmInstance);
+    if (context.method.name === "replaceById") {
+      var roleMappingModel = loopback.getModel("RoleMapping");
 
+      //clear existing roles
+      roleMappingModel.destroyAll(
+        {
+          principalType: "user",
+          principalId: user.id,
+        },
+        function(err, info) {
+          if(err){
+            console.log(err);
+          }
+          console.log(info);
+        }
+      );
+
+      if (!user.roles) {
+        return;
+      }
+
+      user.roles.forEach((role) => {
+        roleMappingModel.upsertWithWhere(
+          {
+            principalType: "user",
+            principalId: user.id,
+            roleId: role,
+          },
+          {
+            principalType: "user",
+            principalId: user.id,
+            roleId: role,
+          },
+          function(err, info) {
+            console.log(info);
+          }
+        );
+      });
     }
-  );
-}
-    console.log('after user update', context);
+    console.log("after user update", context);
 
     next();
   });
 
   // Method to render
-  User.afterRemote('prototype.verify', function (context, user, next) {
-    context.res.render('response', {
-      title: 'A Link to reverify your identity has been sent ' +
-        'to your email successfully',
-      content: 'Please check your email and click on the verification link ' +
-        'before logging in',
-      redirectTo: '/',
-      redirectToLinkText: 'Log in'
+  User.afterRemote("prototype.verify", function (context, user, next) {
+    context.res.render("response", {
+      title:
+        "A Link to reverify your identity has been sent " +
+        "to your email successfully",
+      content:
+        "Please check your email and click on the verification link " +
+        "before logging in",
+      redirectTo: "/",
+      redirectToLinkText: "Log in",
     });
   });
 
   //send password reset link when requested
-  User.on('resetPasswordRequest', function (info) {
-    var url = 'http://' + config.host + ':' + config.port + '/reset-password';
-    var html = 'Click <a href="' + url + '?access_token=' +
-      info.accessToken.id + '">here</a> to reset your password';
+  User.on("resetPasswordRequest", function (info) {
+    var url = "http://" + config.host + ":" + config.port + "/reset-password";
+    var html =
+      'Click <a href="' +
+      url +
+      "?access_token=" +
+      info.accessToken.id +
+      '">here</a> to reset your password';
 
-    User.app.models.Email.send({
-      to: info.email,
-      from: senderAddress,
-      subject: 'Password reset',
-      html: html
-    }, function (err) {
-      if (err) return console.log('> error sending password reset email');
-      console.log('> sending password reset email to:', info.email);
-    });
+    User.app.models.Email.send(
+      {
+        to: info.email,
+        from: senderAddress,
+        subject: "Password reset",
+        html: html,
+      },
+      function (err) {
+        if (err) return console.log("> error sending password reset email");
+        console.log("> sending password reset email to:", info.email);
+      }
+    );
   });
 
   //render UI page after password change
-  User.afterRemote('changePassword', function (context, user, next) {
-    context.res.render('response', {
-      title: 'Password changed successfully',
-      content: 'Please login again with new password',
-      redirectTo: '/',
-      redirectToLinkText: 'Log in'
+  User.afterRemote("changePassword", function (context, user, next) {
+    context.res.render("response", {
+      title: "Password changed successfully",
+      content: "Please login again with new password",
+      redirectTo: "/",
+      redirectToLinkText: "Log in",
     });
   });
 
   //render UI page after password reset
-  User.afterRemote('setPassword', function (context, user, next) {
-    context.res.render('response', {
-      title: 'Password reset success',
-      content: 'Your password has been reset successfully',
-      redirectTo: '/',
-      redirectToLinkText: 'Log in'
+  User.afterRemote("setPassword", function (context, user, next) {
+    context.res.render("response", {
+      title: "Password reset success",
+      content: "Your password has been reset successfully",
+      redirectTo: "/",
+      redirectToLinkText: "Log in",
     });
   });
 };
