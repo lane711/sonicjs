113000645eed013dc5a984f367c57667741668ba
Lane Campbell <ldc0618@gmail.com>
2020-09-28 19:58:24 -0700

    .

--- cypress/integration/newsite_spec.js
+++ cypress/integration/newsite_spec.js
@@ -2,16 +2,26 @@ const { expect } = require("chai");
 const { iteratee } = require("lodash");
 
 describe("new site", function () {
+
+  before(() => {
+    //clear admin user
+  })
+
   it("new site should redirect to register page", function () {
     cy.visit('http://localhost:3018/');
     cy.contains('Register');
     cy.contains('Create your admin account');
   });
 
-  it("register admin account", function () {
+  it("register form validation", function () {
     cy.visit('http://localhost:3018/');
 
 
     cy.contains('Create Account').click();
+
+    cy.get('[type="email"]').then(($input) => {
+      expect($input[0].validationMessage).to.eq('I expect an email!')
+    })
+
   });
 });
