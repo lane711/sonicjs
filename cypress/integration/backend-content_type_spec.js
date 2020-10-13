836f8a132a5bf1a293d0a1205836e09b27a8aef5
Lane Campbell <ldc0618@gmail.com>
2020-10-12 20:29:16 -0700

    content type edit delete

---
+++ cypress/integration/backend-content_type_spec.js
@@ -0,0 +1,45 @@
+const { expect } = require("chai");
+const { iteratee } = require("lodash");
+
+describe("Admin Content Types", function () {
+  beforeEach(() => {
+    cy.SonicJs.login();
+  });
+
+  it("Content type create", function () {
+    cy.visit(`${cy.SonicJs.getBaseUrl()}/admin/content-types`);
+    cy.contains("New Content Type").click();
+    cy.get('input[name="data[title]"]').type("Cypress CT");
+    // cy.wait(5000);
+
+    cy.get('input[name="data[systemid]"]').type("cypress-content-type");
+    cy.contains("Create").click();
+    cy.contains("Cypress CT");
+    //
+    cy.url().should(
+      "include",
+      "/admin/content-types/edit/cypress-content-type"
+    );
+  });
+
+  it("Content type edit", function () {
+
+    cy.visit(`${cy.SonicJs.getBaseUrl()}/admin/content-types/edit/cypress-content-type`);
+    cy.get('input[name="data[title]"]').type(' EDITED');
+    // cy.wait(3000);
+
+    cy.contains('Save Title & SystemId').click();
+
+    cy.wait(1000);
+    cy.visit(`${cy.SonicJs.getBaseUrl()}/admin/content-types/edit/cypress-content-type`);
+    cy.contains('Cypress CT EDITED');
+
+  });
+
+  it("Content type delete", function () {
+    cy.visit(`${cy.SonicJs.getBaseUrl()}/admin/content-types`);
+    cy.get('[aria-label="Delete Cypress CT EDITED"]').first().click();
+    cy.wait(500);
+    cy.contains("Confirm Delete").click();
+  });
+});
