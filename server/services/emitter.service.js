259309f170b2eeed740941438eb8c1cde1a636af
Lane Campbell <ldc0618@gmail.com>
2020-10-20 20:09:59 -0700

    refac emitter

--- server/services/emitter.service.js
+++ server/services/emitter.service.js
@@ -1,81 +1,37 @@
 var emitterList = [];
 
-module.exports = {
+(function (exports) {
+  exports.on = function (emitterName, functionToExecute) {
+    // console.log('>> on ' + emitterName, functionToExecute);
+    emitterList.push({
+      emitterName: emitterName,
+      functionToExecute: functionToExecute,
+    });
+  };
 
-    on: function (emitterName, functionToExecute) {
-        // console.log('>> on ' + emitterName, functionToExecute);
-        emitterList.push({ emitterName: emitterName, functionToExecute: functionToExecute })
-    },
+  exports.once = function (emitterName, options) {
+    // console.log('>> once ' + emitterName);
+  };
 
-    once: function (emitterName, options) {
-        // console.log('>> once ' + emitterName);
-    },
+  exports.emit = async function (emitterName, options) {
+    // console.log('*** emit >> ' + emitterName);
+    // console.log('list', emitterList);
 
-    emit: async function (emitterName, options) {
-        // console.log('*** emit >> ' + emitterName);
-        // console.log('list', emitterList);
+    //TODO: ordering
 
-        //TODO: ordering
+    //filter by current emiiterName
+    let emitters = emitterList.filter((x) => x.emitterName === emitterName);
 
-        //filter by current emiiterName
-        let emitters = emitterList.filter(x => x.emitterName === emitterName);
+    //execute the functions
+    for (let index = 0; index < emitters.length; index++) {
+      let subscriber = emitters[index];
+      // console.log('executing...' + subscriber.functionToExecute, "on " + subscriber.emitterName);
 
-        //execute the functions
-        for (let index = 0; index < emitters.length; index++) {
-            let subscriber = emitters[index];
-            // console.log('executing...' + subscriber.functionToExecute, "on " + subscriber.emitterName);
-
-            try {
-                await subscriber.functionToExecute(options);
-            } catch (error) {
-                console.log('EMIT ERROR:', error);
-            }
-        }
-    },
-    
-}
-
-
-// (function(exports) {
-//   exports.test = function() {
-//     return "hello world";
-//   };
-
-//   exports.generateShortCode = function(name, args) {
-//     let sc = `[${name.toUpperCase()}`;
-//     if (args) {
-//       for (var key in args) {
-//         // console.log(' name=' + key + ' value=' + args[key]);
-//         sc += ` ${key}="${args[key]}"`;
-//       }
-//     }
-//     sc += "]";
-//     return sc;
-//   };
-
-//   exports.generateShortCodeList = function(list) {
-//     let shortCodeList = "";
-//     list.forEach(shortCode => {
-//       shortCodeList += this.generateShortCode(shortCode.module, {
-//         id: shortCode.id
-//       });
-//     });
-//     return shortCodeList;
-//   };
-
-//   exports.generateContentFromShortcodeList = function(shortCodeList) {
-//     let shortCodeContent = "";
-//     shortCodeList.children.forEach(sc => {
-//       shortCodeContent += sc.shortcode.codeText;
-//     });
-//     return shortCodeContent;
-//   };
-
-//   exports.getBaseUrl = function(){
-//     var url = window.location.href;
-//     var arr = url.split("/");
-//     var result = arr[0] + "//" + arr[2]
-//     return result;
-//   };
-
-// })(typeof exports === "undefined" ? (this["sharedService"] = {}) : exports);
+      try {
+        await subscriber.functionToExecute(options);
+      } catch (error) {
+        console.log("EMIT ERROR:", error);
+      }
+    }
+  };
+})(typeof exports === "undefined" ? (this["emitterService"] = {}) : exports);
