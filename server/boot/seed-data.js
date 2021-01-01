922e3efc717e8e8bd3b88cb826b5ee6fda973c0d
Lane Campbell <ldc0618@gmail.com>
2021-01-01 14:37:36 -0800

    update seeding

--- server/boot/seed-data.js
+++ server/boot/seed-data.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const { parse, stringify } = require("envfile");
 const path = require("path");
-const Content = require("../models/content");
+const Content = require("../schema/models/content");
 
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/sonicjs', {
+mongoose.connect("mongodb://localhost:27017/sonicjs", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 mongoose.connection.once("open", () => {
@@ -18,70 +18,85 @@ mongoose.connection.once("open", () => {
   let dataRaw = fs.readFileSync("server/data/data.json");
   let data = JSON.parse(dataRaw);
 
-// console.log(data);
+  // console.log(data);
   // migrateContentTypes(app);
   migrateContent(data);
-
 });
 
-  async function migrateContent(data) {
+async function migrateContent(data) {
+  let contentObjs = data.models.content;
 
-      let contentObjs = data.models.content;
+  var contents = Object.keys(contentObjs).map((key) => [
+    Number(key),
+    contentObjs[key],
+  ]);
 
-      var contents = Object.keys(contentObjs).map((key) => [
-        Number(key),
-        contentObjs[key],
-      ]);
+  contents.forEach((contentData) => {
+    console.log(contentData);
+    let objString = contentData[1];
+    let obj = JSON.parse(objString);
 
-      contents.forEach((contentData) => {
-        console.log(contentData);
-        let objString = contentData[1];
-        let obj = JSON.parse(objString);
+    let newContent = {
+      data: obj.data,
+    };
 
-        let newContent = {
-          data: obj.data,
-        };
+    console.log(newContent);
 
-        console.log(newContent);
+    let url = newContent.data.url ?? slugify(newContent.data.title) ?? slugify(newContent.data.contentType);
+    console.log("url -->", url);
 
-        //insert to db
-        let content = new Content({
-          contentTypeId: newContent.data.contentType,
-          data: newContent.data,
-          createdByUserId: -1,
-          lastUpdatedByUserId: -1,
-          createdOn: new Date(),
-          updatedOn: new Date()
-        });
-
-        delete content.data.contentType;
-        
-        try {
-          content.save();
-
-        } catch (error) {
-          console.log(err);
-        }
-        
-        // app.models.content.create(newContent, function (err, newInstance) {
-        //   if (err) throw err;
-        //   console.log("Content created:", newInstance);
-        //   setEnvVarToEnsureMigrationDoesNotRunAgain();
-        //   console.log("Success! Initial data migration complete.");
-        // });
-      });
-  }
-
-  async function setEnvVarToEnsureMigrationDoesNotRunAgain() {
-    let sourcePath = path.join(__dirname, "../..", ".env");
-
-    fs.readFile(sourcePath, "utf8", function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      let parsedFile = parse(data);
-      parsedFile.RUN_NEW_SITE_MIGRATION = "FALSE";
-      fs.writeFileSync(sourcePath, stringify(parsedFile));
+    //insert to db
+    let content = new Content({
+      contentTypeId: newContent.data.contentType,
+      data: newContent.data,
+      createdByUserId: -1,
+      lastUpdatedByUserId: -1,
+      url: url,
+      createdOn: new Date(),
+      updatedOn: new Date(),
     });
+
+    delete content.data.contentType;
+
+    try {
+      content.save();
+    } catch (error) {
+      console.log(err);
+    }
+
+    // app.models.content.create(newContent, function (err, newInstance) {
+    //   if (err) throw err;
+    //   console.log("Content created:", newInstance);
+    //   setEnvVarToEnsureMigrationDoesNotRunAgain();
+    //   console.log("Success! Initial data migration complete.");
+    // });
+  });
+}
+
+function slugify(text) {
+  // console.log('slug', text);
+  if (!text) {
+    return undefined;
   }
+
+  let slug = text
+    .toLowerCase()
+    .replace(/[^\w ]+/g, "")
+    .replace(/ +/g, "-");
+
+  return '/' + slug;
+}
+
+async function setEnvVarToEnsureMigrationDoesNotRunAgain() {
+  let sourcePath = path.join(__dirname, "../..", ".env");
+
+  fs.readFile(sourcePath, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    let parsedFile = parse(data);
+    parsedFile.RUN_NEW_SITE_MIGRATION = "FALSE";
+    fs.writeFileSync(sourcePath, stringify(parsedFile));
+  });
+}
 // };
