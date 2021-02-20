ecb5d13398bd83ceaf653c129de48d8848a564dd
Lane Campbell <ldc0618@gmail.com>
2021-02-20 08:29:31 -0800

    cleanup

--- server/z_models/model-utils.js
+++
@@ -1,9 +0,0 @@
-exports.clearBaseACLs = function (ModelType, ModelConfig) {
-  // console.log('user acls:', ModelType.settings.acls.length);
-  ModelType.settings.acls.length = 0;
-  ModelConfig.acls.forEach(function (r) {
-    ModelType.settings.acls.push(r);
-  });
-  // console.log('user acls after:', ModelType.settings.acls.length);
-
-};
