412ecaaf42917ddcd1f4e07d5bcad8a7bf2867f4
Lane Campbell <ldc0618@gmail.com>
2021-02-24 07:49:02 -0800

    fix url update

--- server/modules/blog/services/blog-main-service.js
+++ server/modules/blog/services/blog-main-service.js
@@ -1,25 +1,3 @@
-// var dataService = require('../../../services/data.service');
-// var emitterService = require('../../../services/emitter.service');
-// var globalService = require('../../../services/global.service');
-
-// module.exports = blogMainService = {
-
-//     startup: async function () {
-//         emitterService.on('beginProcessModuleShortCode', async function (options) {
-
-//             if (options.shortcode.name === 'BLOG') {
-
-//                 options.moduleName = 'blog';
-//                 await moduleService.processModuleInColumn(options);
-//             }
-
-//         });
-//     },
-
-// }
-
-
-
 var dataService = require("../../../services/data.service");
 var emitterService = require("../../../services/emitter.service");
 var globalService = require("../../../services/global.service");
