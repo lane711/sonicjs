b24e44643fab1373f3745b953b0ac372683a43f3
Lane Campbell <ldc0618@gmail.com>
2021-03-02 08:14:23 -0800

    load menu dta

--- server/modules/menu-accordion/services/menu-accordion-main-service.js
+++ server/modules/menu-accordion/services/menu-accordion-main-service.js
@@ -1,6 +1,7 @@
 var dataService = require('../../../services/data.service');
 var emitterService = require('../../../services/emitter.service');
 var globalService = require('../../../services/global.service');
+var menuService = require('../../../services/menu.service');
 
 module.exports = menuAccordionMainService = {
 
@@ -10,10 +11,28 @@ module.exports = menuAccordionMainService = {
             if (options.shortcode.name === 'MENU-ACCORDION') {
 
                 options.moduleName = 'menu-accordion';
+
+
                 await moduleService.processModuleInColumn(options);
             }
 
         });
+
+
+        emitterService.on('postModuleGetData', async function (options) {
+
+            if (options.shortcode.name === 'MENU-ACCORDION') {
+      
+              //TODO: how to delay processing of the module until after the module list is populated?
+                // titleModules.push(options.viewModel.data);
+                let menuToDisplay = options.viewModel.data.menuTitle;
+                let menuData = await menuService.getMenu(menuToDisplay);
+                options.viewModel.data.menuData = menuData;
+
+            }
+            
+        });
+        
     },
 
 }
