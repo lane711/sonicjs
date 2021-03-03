c3ef6d390c5de7daeab91695edbd7ce5f9d77289
Lane Campbell <ldc0618@gmail.com>
2021-03-03 08:54:27 -0800

    add page templates module

---
+++ server/modules/page-templates/services/page-templates-main-service.js
@@ -0,0 +1,20 @@
+var dataService = require('../../../services/data.service');
+var emitterService = require('../../../services/emitter.service');
+var globalService = require('../../../services/global.service');
+
+module.exports = pageTemplatesMainService = {
+
+    startup: async function () {
+        emitterService.on('beginProcessModuleShortCode', async function (options) {
+
+            if (options.shortcode.name === 'PAGE-TEMPLATES') {
+
+                options.moduleName = 'page-templates';
+                await moduleService.processModuleInColumn(options);
+            }
+
+        });
+    },
+
+}
+
