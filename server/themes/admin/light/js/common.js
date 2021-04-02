452e06fe3139b4ea4907d295417bc88db7875033
Lane Campbell <ldc0618@gmail.com>
2021-04-02 09:15:09 -0700

    update data service requests

--- server/themes/admin/light/js/common.js
+++ server/themes/admin/light/js/common.js
@@ -1,10 +1,9 @@
 $(document).ready(async function () {
-
   $(function () {
-    $('[data-toggle="popover"]').popover()
-  })
+    $('[data-toggle="popover"]').popover();
+  });
 
-  $('table').on('shown.bs.popover', function () {
+  $("table").on("shown.bs.popover", function () {
     // debugger;
     var btns = document.getElementsByClassName("custom-delete");
     for (var i = 0; i < btns.length; i++) {
@@ -40,31 +39,26 @@ $(document).ready(async function () {
             await dataService.deleteModule(idToDelete);
             location.reload();
           }
-
         }
-
       };
     }
-
   });
 
   function getPathParts(path, positionFromLast) {
-    var parts = path.split('/');
+    var parts = path.split("/");
     return parts[parts.length - (positionFromLast + 1)];
   }
 
-  $('#admin-content').DataTable(
-    {
-      "order": [[ 2, "desc" ]]
-  }
-  );
-
+  $("#admin-content").DataTable({
+    order: [[2, "desc"]],
+  });
 });
 
 function wait(ms) {
   var d = new Date();
   var d2 = null;
-  do { d2 = new Date(); }
-  while (d2 - d < ms);
+  do {
+    d2 = new Date();
+  } while (d2 - d < ms);
 }
 
