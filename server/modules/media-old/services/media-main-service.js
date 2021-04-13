f2fd7a0205b77d96c47851f9680fe3aa5c6fec76
Lane Campbell <ldc0618@gmail.com>
2021-04-13 06:48:09 -0700

    add new media module

---
+++ server/modules/media-old/services/media-main-service.js
@@ -0,0 +1,37 @@
+var dataService = require('../../../services/data.service');
+var emitterService = require('../../../services/emitter.service');
+var globalService = require('../../../services/global.service');
+var viewService = require('../../../services/view.service');
+var mediaService = require('../../../services/media.service');
+
+
+
+module.exports = mediaMainService = {
+
+    startup: async function () {
+        emitterService.on('beginProcessModuleShortCode', async function (options) {
+
+            if (options.shortcode.name === 'MODULE-MEDIA') {
+                let id = options.shortcode.properties.id;
+                let contentType = options.shortcode.properties.contentType;
+                let viewPath = __dirname + `/../views/media-main.handlebars`;
+                let viewModel = await dataService.getContentById(id);
+                let files = await dataService.getContentByContentTypeAndTag('media', viewModel.data.tags);
+                let sortedFiles = files.sort((a, b) => (a.data.sortOrder > b.data.sortOrder) ? 1 : -1)
+                await mediaService.addMediaUrl(sortedFiles);
+                viewModel.data.files = sortedFiles;
+                let processedHtml = await mediaMainService.processView(contentType, viewModel, viewPath);
+
+                options.page.data.html = options.page.data.html.replace(options.shortcode.codeText, processedHtml);
+            }
+
+        });
+    },
+
+    processView: async function (contentType, viewModel, viewPath) {
+        var result = await viewService.getProcessedView(contentType, viewModel, viewPath);
+
+        return result;
+    }
+
+}
