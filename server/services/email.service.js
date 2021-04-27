b848c97ec65fe51546aeb2242e8dcdb33e2c06f4
Lane Campbell <ldc0618@gmail.com>
2021-04-27 15:18:15 -0700

    fix contact forms

--- server/services/email.service.js
+++ server/services/email.service.js
@@ -8,17 +8,17 @@ const ShortcodeTree = require('shortcode-tree').ShortcodeTree;
 const chalk = require('chalk');
 const log = console.log;
 var path = require("path");
+var sendgrid = require('sendgrid').mail;
 
 module.exports = emailService = {
 
 
     sendEmail: async function (from, to, subject, body) {
 
-        var helper = require('sendgrid').mail;
-                var from_email = new helper.Email(from);
-                var to_email = new helper.Email(to);;
-                var content = new helper.Content('text/plain', body);
-                var mail = new helper.Mail(from_email, subject, to_email, content);
+                var from_email = new sendgrid.Email(from);
+                var to_email = new sendgrid.Email(to);;
+                var content = new sendgrid.Content('text/plain', body);
+                var mail = new sendgrid.Mail(from_email, subject, to_email, content);
 
                 if(!process.env.SENDGRID_API_KEY){
                     throw ('SENDGRID_API_KEY not set!');
