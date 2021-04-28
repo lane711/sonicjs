c50ce877c09a4a239ff78bada75f2fce6e9a911d
Lane Campbell <ldc0618@gmail.com>
2021-04-28 07:59:48 -0700

    about page styles

--- server/modules/menu-title/services/menu-title-main-service.js
+++ server/modules/menu-title/services/menu-title-main-service.js
@@ -6,67 +6,68 @@ var sourceColumnId = undefined;
 var titleModules = [];
 
 module.exports = menuTitleMainService = {
-  startup: async function() {
+  startup: async function () {
     // console.log('--> startup on menuTitleMainService');
 
-    emitterService.on("postProcessPage", async function(options) {
-        // console.log('resetting titeModules');
-        titleModules = [];
+    // emitterService.on("beginProcessModuleShortCode", async function (options) {
+    //   if (options.shortcode.name === "MENU-TITLE") {
+    //     options.moduleName = "html";
+    //     await moduleService.processModuleInColumn(options);
+    //   }
+    // });
 
+    emitterService.on("postProcessPage", async function (options) {
+      // console.log('resetting titeModules');
+      titleModules = [];
     });
 
-    emitterService.on("beginProcessModuleShortCodeDelayed", async function(options) {
-      if (options.shortcode.name === "MENU-TITLE") {
-        //TODO: don't process shortcode so that it can be processed after target columns has been built
-        options.moduleName = "menu-title";
+    emitterService.on(
+      "beginProcessModuleShortCodeDelayed",
+      async function (options) {
+        if (options.shortcode.name === "MENU-TITLE") {
+          //TODO: don't process shortcode so that it can be processed after target columns has been built
+          options.moduleName = "menu-title";
 
-        // console.log('beginProcessModuleShortCodeDelayed',titleModules.length);
-        // options.viewModel = {data : {headerTags: titleModules}};
-        // console.log('tags', options.viewModel.data.headerTags);
+          // console.log('beginProcessModuleShortCodeDelayed',titleModules.length);
+          // options.viewModel = {data : {headerTags: titleModules}};
+          // console.log('tags', options.viewModel.data.headerTags);
 
-        await moduleService.processModuleInColumn(options);
-
-
+          await moduleService.processModuleInColumn(options);
+        }
       }
-    });
+    );
 
-    emitterService.on('postModuleGetData', async function (options) {
-
-      if (options.shortcode.name === 'TITLE') {
-
+    emitterService.on("postModuleGetData", async function (options) {
+      if (options.shortcode.name === "TITLE") {
         //TODO: how to delay processing of the module until after the module list is populated?
-          titleModules.push(options.viewModel.data);
+        titleModules.push(options.viewModel.data);
       }
 
-      if (options.shortcode.name === 'MENU-TITLE') {
-
+      if (options.shortcode.name === "MENU-TITLE") {
         //now we should have a complete list of title modules
-          // console.log('postModuleGetData',titleModules.length);
+        // console.log('postModuleGetData',titleModules.length);
 
-          options.viewModel.data.headerTags = titleModules;
-          options.viewModel.data.pageUrl = options.req.path;
+        options.viewModel.data.headerTags = titleModules;
+        options.viewModel.data.pageUrl = options.req.path;
 
-          // var headerTagsArr = Array.from(titleModules);
-          // const names = titleModules.map(item => item.text)
+        // var headerTagsArr = Array.from(titleModules);
+        // const names = titleModules.map(item => item.text)
 
-          // for (var titleModule of titleModules){
-          //   options.viewModel.data.headerTags.push(titleModule);
-          // }
+        // for (var titleModule of titleModules){
+        //   options.viewModel.data.headerTags.push(titleModule);
+        // }
 
-          // console.log(options.viewModel.data.headerTags);
+        // console.log(options.viewModel.data.headerTags);
       }
+    });
 
-  });
-
-
-
-    emitterService.on("preRender", async function(options) {
+    emitterService.on("preRender", async function (options) {
       let x = sourceColumnId;
       options.page.data.html;
     });
   },
 
-  findColumnByHtmlId: async function(section, columnId) {
+  findColumnByHtmlId: async function (section, columnId) {
     for (const row of section.data.rows) {
       for (const column of row.columns) {
         if (column.id === columnId) {
@@ -74,6 +75,5 @@ module.exports = menuTitleMainService = {
         }
       }
     }
-  }
-
+  },
 };
