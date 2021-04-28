3935d3b75c4ccbc9d50975166469620e170866bd
Lane Campbell <ldc0618@gmail.com>
2021-04-28 16:23:27 -0700

    Revert "rm user module"
    This reverts commit 9fcf0d431f763079da66cba801d152bf7fbc3e49.

---
+++ server/modules/user/services/user-main-service.js
@@ -0,0 +1,14 @@
+var dataService = require("../../../services/data.service");
+var emitterService = require("../../../services/emitter.service");
+var globalService = require("../../../services/global.service");
+
+var sourceColumnId = undefined;
+var titleModules = [];
+
+module.exports = menuTitleMainService = {
+  startup: async function() {
+
+
+  },
+
+};
