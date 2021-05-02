e2e57660b283ca8ce80bd05bd8961917100c7f46
Lane Campbell <ldc0618@gmail.com>
2021-05-02 11:40:35 -0700

    add comment module

---
+++ server/modules/comment/services/comment-main-service.js
@@ -0,0 +1,34 @@
+var dataService = require('../../../services/data.service');
+var emitterService = require('../../../services/emitter.service');
+var globalService = require('../../../services/global.service');
+
+module.exports = commentMainService = {
+
+    startup: async function () {
+        emitterService.on('beginProcessModuleShortCode', async function (options) {
+
+            if (options.shortcode.name === 'COMMENT') {
+
+                options.moduleName = 'comments';
+                await moduleService.processModuleInColumn(options);
+            }
+
+        });
+
+        emitterService.on("postModuleGetData", async function (options) {
+            if (options.shortcode.name !== "COMMENT") {
+              return;
+            }
+      
+            options.viewModel.data.form = await formService.getForm(
+              "comment",
+              undefined,
+              "submitForm(submission)"
+            );
+      
+            // console.log('contact module after view model', options.viewModel);
+          });
+    },
+
+}
+
