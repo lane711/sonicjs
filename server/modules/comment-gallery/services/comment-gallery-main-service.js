bb6f7b3d8d7b944a4335f4a6a9f1ad87a8d798b9
Lane Campbell <ldc0618@gmail.com>
2021-05-02 17:52:09 -0700

    add comment gallery

---
+++ server/modules/comment-gallery/services/comment-gallery-main-service.js
@@ -0,0 +1,67 @@
+var dataService = require('../../../services/data.service');
+var emitterService = require('../../../services/emitter.service');
+var globalService = require('../../../services/global.service');
+
+module.exports = commentGalleryMainService = {
+
+    startup: async function () {
+        emitterService.on('beginProcessModuleShortCode', async function (options) {
+
+            if (options.shortcode.name === 'COMMENT-GALLERY') {
+
+                options.moduleName = 'comment-gallery';
+                await moduleService.processModuleInColumn(options);
+            }
+
+        });
+
+        emitterService.on("postModuleGetData", async function (options) {
+            if (options.shortcode.name === "COMMENT-GALLERY") {
+              await blogMainService.getCommentData(options);
+            }
+          });
+    
+    },
+
+    getCommentData: async function (options) {
+        let id = options.shortcode.properties.id;
+        let moduleData = await dataService.getContentById(id);
+        let contentType = "comment";
+        let viewModel = moduleData;
+    
+        let listRaw = await dataService.getContentByType(contentType);
+    
+        listRaw = listRaw.filter((x) => x.data.title);
+        let list = listRaw.map(function (record) {
+          return {
+            data: {
+              title: record.data.title,
+              body: formatService.stripHtmlTags(
+                helperService.truncateString(record.data.body, 400)
+              ),
+              image: record.data.fileName
+                ? dataService.getImageUrl(record.data.fileName)
+                : undefined,
+              url: record.data.url,
+              createdOn: record.data.createdOn,
+            },
+          };
+        });
+    
+    
+        options.viewModel.data.list = list;
+      },
+    
+      processView: async function (contentType, viewModel, viewPath) {
+        var result = await viewService.getProcessedView(
+          contentType,
+          viewModel,
+          viewPath
+        );
+    
+        return result;
+      },
+    
+
+}
+
--- server/modules/comment/services/comment-main-service.js
+++ server/modules/comment/services/comment-main-service.js
@@ -1,34 +1,57 @@
-var dataService = require('../../../services/data.service');
-var emitterService = require('../../../services/emitter.service');
-var globalService = require('../../../services/global.service');
+var dataService = require("../../../services/data.service");
+var emitterService = require("../../../services/emitter.service");
+var globalService = require("../../../services/global.service");
 var formService = require("../../../services/form.service");
 
 module.exports = commentMainService = {
+  startup: async function () {
+    emitterService.on("beginProcessModuleShortCode", async function (options) {
+      if (options.shortcode.name === "COMMENT") {
+        options.moduleName = "comment";
+        await moduleService.processModuleInColumn(options);
+      }
+    });
 
-    startup: async function () {
-        emitterService.on('beginProcessModuleShortCode', async function (options) {
+    emitterService.on("postModuleGetData", async function (options) {
+      if (options.shortcode.name !== "COMMENT") {
+        return;
+      }
 
-            if (options.shortcode.name === 'COMMENT') {
+      options.viewModel.data.form = await formService.getForm(
+        "comment",
+        undefined,
+        "submitForm(submission)"
+      );
+    });
 
-                options.moduleName = 'comment';
-                await moduleService.processModuleInColumn(options);
-            }
+    emitterService.on("afterFormSubmit", async function (options) {
+      if (options.data.contentType !== "comment") {
+        return;
+      }
 
-        });
+      // save the form
+      await dataService.contentCreate(options);
 
-        emitterService.on("postModuleGetData", async function (options) {
-            if (options.shortcode.name !== "COMMENT") {
-              return;
-            }
-      
-            options.viewModel.data.form = await formService.getForm(
-              "comment",
-              undefined,
-              "submitForm(submission)"
-            );
-      
-          });
-    },
+      // send the emails
+      let contact = options.data;
 
-}
+      //confirmation to user
+      let body = `Hi ${contact.name}, \n\nThanks for reaching out. We'll get back to you ASAP.\n\nFor your reference, here was your message:\n${contact.message}`;
+      await emailService.sendEmail(
+        contact.email,
+        "lane@sonicjs.com",
+        "SoncisJs Message Received",
+        body
+      );
 
+      //admin notification
+      let adminBody = `${contact.name} (${contact.email}) wrote: \n\n${contact.message}`;
+      await emailService.sendEmail(
+        "lane@sonicjs.com",
+        contact.email,
+        "SoncisJs Contact",
+        adminBody
+      );
+    });
+  },
+};
