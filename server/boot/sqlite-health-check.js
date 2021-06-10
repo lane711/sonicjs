bad506c17c536723be1173b0ff70be7d47509148
Lane Campbell <ldc0618@gmail.com>
2021-06-10 11:59:53 -0700

    hard code site colors

--- server/boot/sqlite-health-check.js
+++ server/boot/sqlite-health-check.js
@@ -5,5 +5,4 @@ typeorm.createConnection().then(async (connection) => {
   const contentRepo = connection.getRepository(Content);
   let contents = await contentRepo.find();
   console.log(`Success, content doc count: ${contents.length}`);
-
 });
