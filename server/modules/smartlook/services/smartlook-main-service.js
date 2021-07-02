44abcfd10def35aa5711317215bb483052a8054b
Lane Campbell <ldc0618@gmail.com>
2021-07-01 20:28:16 -0700

    add smartlook env

--- server/modules/smartlook/services/smartlook-main-service.js
+++ server/modules/smartlook/services/smartlook-main-service.js
@@ -14,17 +14,16 @@ module.exports = smartlookMainService = {
   },
 
   addHeaderJs: async function (options) {
-    let smartlookSettings = await dataService.getContentTopOne("smartlook", options.req.sessionID);
-    if (smartlookSettings && process.env.ENABLE_SMARTLOOK === "TRUE") {
+    // let smartlookSettings = await dataService.getContentTopOne("smartlook", options.req.sessionID);
+    if (process.env.SMARTLOOK_CLEINTID) {
       options.page.data.headerJs += `<script type='text/javascript'>
           window.smartlook||(function(d) {
             var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
             var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
             c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
             })(document);
-            smartlook('init', '${smartlookSettings.data.clientId}');
+            smartlook('init', '${process.env.SMARTLOOK_CLEINTID}');
         </script>`;
     }
   },
 };
-smartlook
