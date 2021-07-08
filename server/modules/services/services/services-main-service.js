c4c6881079c8f2e297bfdffdc6e89a9d2d4c0517
Lane Campbell <ldc0618@gmail.com>
2021-07-08 13:12:56 -0700

    update services service

--- server/modules/services/services/services-main-service.js
+++ server/modules/services/services/services-main-service.js
@@ -14,7 +14,57 @@ module.exports = servicesMainService = {
             }
 
         });
+
+
+        emitterService.on("postModuleGetData", async function (options) {
+            if (options.shortcode.name === "SERVICES") {
+              await servicesMainService.getServicesData(options);
+            }
+          });
     },
+
+    getServicesData: async function (options) {
+        let id = options.shortcode.properties.id;
+        let moduleData = await dataService.getContentById(id, options.req.sessionID);
+        let contentType = "services";
+        let viewModel = moduleData;
+    
+        let listRaw = await dataService.getContentByType(contentType, options.req.sessionID);
+        listRaw = listRaw.sort((a, b) => (a.title > b.title) ? 1 : -1)
+    
+        // listRaw = listRaw.filter((x) => x.data.title);
+        // let list = listRaw.map(function (record) {
+        //   return {
+        //     data: {
+        //       title: record.data.title,
+        //       body: formattingService.stripHtmlTags(
+        //         helperService.truncateString(record.data.body, 400)
+        //       ),
+        //       image: record.data.fileName
+        //         ? dataService.getImageUrl(record.data.fileName)
+        //         : undefined,
+        //       url: record.data.url,
+        //       createdOn: record.createdOn,
+        //     },
+        //   };
+        // });
+    
+        // let sortedList = list.sort((a, b) => (a.data.createdOn < b.data.createdOn) ? 1 : -1)
+        // await formattingService.formatDates(list, true);
+    
+        // viewModel.list = list;
+        // let viewPath = `/server/modules/blog/views/blog-main.hbs`;
+        // // let sortedFiles = files.sort((a, b) => (a.data.sortOrder > b.data.sortOrder) ? 1 : -1)
+        // // await mediaService.addMediaUrl(sortedFiles);
+        // // viewModel.data.files = sortedFiles;
+        // let processedHtml = await blogMainService.processView(
+        //   contentType,
+        //   viewModel,
+        //   viewPath
+        // );
+    
+        options.viewModel.data.list = listRaw;
+      },
 
 }
 
