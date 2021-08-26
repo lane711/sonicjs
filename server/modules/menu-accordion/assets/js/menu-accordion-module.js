97a3dccc84c8c8c6084adee01d921ebab7eb78c5
lane711 <ldc0618@gmail.com>
2021-08-26 10:28:43 -0700

    add image example

--- server/modules/menu-accordion/assets/js/menu-accordion-module.js
+++ server/modules/menu-accordion/assets/js/menu-accordion-module.js
@@ -1,21 +1,25 @@
 // JS File for Module: menu-accordion
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {scrollCheck()};
+window.onscroll = function () {
+  scrollCheck();
+};
 
 // Get the navbar
 var navbar = document.getElementById("accordian-menu");
 
 // Get the offset position of the navbar
-if(navbar){
+if (navbar) {
   var sticky = navbar.getBoundingClientRect().top - 90;
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function scrollCheck() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  if (navbar) {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
 }
