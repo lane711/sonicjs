ac86c31557c6eb9e7c9cd86db76d3c3e081d60d2
lane711 <ldc0618@gmail.com>
2021-09-14 19:45:08 -0700

    add newsletter module

--- server/modules/newsletter/services/newsletter-main-service.js
+++ server/modules/newsletter/services/newsletter-main-service.js
@@ -1,60 +1,20 @@
-var dataService = require("../../../services/data.service");
-var emitterService = require("../../../services/emitter.service");
-var globalService = require("../../../services/global.service");
-var cryptoService = require("../../../services/crypto.service");
-var sendEmail = process.env.SEND_EMAIL;
+var dataService = require('../../../services/data.service');
+var emitterService = require('../../../services/emitter.service');
+var globalService = require('../../../services/global.service');
 
 module.exports = newsletterMainService = {
-  startup: async function (app) {
-    emitterService.on("beginProcessModuleShortCode", async function (options) {
-      if (options.shortcode.name === "NEWSLETTER") {
-        options.moduleName = "newsletter";
-        await moduleService.processModuleInColumn(options);
-      }
-    });
 
-    emitterService.on("afterFormSubmit", async function (options) {
-      if (options.data.contentType !== "newsletter") {
-        return;
-      }
+    startup: async function () {
+        emitterService.on('beginProcessModuleShortCode', async function (options) {
 
-      let encryptedEmail = cryptoService.encrypt(options.email);
+            if (options.shortcode.name === 'NEWSLETTER') {
 
-      // let testDecrypt = cryptoService.decrypt(encryptedEmail);
+                options.moduleName = 'newsletter';
+                await moduleService.processModuleInColumn(options);
+            }
 
-      let payload = {
-        data: {
-          contentType: options.contentType,
-          email: encryptedEmail,
-        },
-      };
+        });
+    },
 
-      // save the form
-      await dataService.contentCreate(payload);
+}
 
-      if (sendEmail === "TRUE") {
-        // send the emails
-
-        //confirmation to user
-        let body = `Hi Fellow Patriot, \n\nThanks for signing up for our mailing list. We'll be in touch soon!`;
-        await emailService.sendEmail(
-          "admin@ocunite.org",
-          "Test 234",
-          options.email,
-          "Newsletter SignUp Confirmed",
-          body
-        );
-
-        //admin notification
-        let adminBody = `${options.email}`;
-        await emailService.sendEmail(
-          options.email,
-          "Test 234",
-          "admin@ocunite.org",
-          "OCUnite Newsletter SignUp",
-          adminBody
-        );
-      }
-    });
-  },
-};
