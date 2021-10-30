142114a452c1a2cbc50038d6edb1e817cec5131d
lane711 <ldc0618@gmail.com>
2021-10-29 18:20:05 -0700

    add sidebar tags list

--- server/modules/taxonomy/services/taxonomy-main-service.js
+++ server/modules/taxonomy/services/taxonomy-main-service.js
@@ -1,20 +1,41 @@
-var dataService = require('../../../services/data.service');
-var emitterService = require('../../../services/emitter.service');
-var globalService = require('../../../services/global.service');
+var dataService = require("../../../services/data.service");
+var emitterService = require("../../../services/emitter.service");
+var globalService = require("../../../services/global.service");
 
 module.exports = taxonomyMainService = {
+  startup: async function (app) {
+    emitterService.on("beginProcessModuleShortCode", async function (options) {
+      if (options.shortcode.name === "TAXONOMY") {
+        options.moduleName = "taxonomy";
+        await moduleService.processModuleInColumn(options);
+      }
 
-    startup: async function () {
-        emitterService.on('beginProcessModuleShortCode', async function (options) {
+      emitterService.on("postModuleGetData", async function (options) {
+        if (options.shortcode.name === "TAXONOMY") {
 
-            if (options.shortcode.name === 'TAXONOMY') {
+          let list = await dataService.getContentById(options.viewModel.data.taxonomy);
 
-                options.moduleName = 'taxonomy';
-                await moduleService.processModuleInColumn(options);
-            }
+          options.viewModel.data.list = list.data.terms;
+        }
+      });
+    });
 
-        });
-    },
+    app.get("/taxonomy-get*", async function (req, res) {
 
-}
+        if(req.url === '/taxonomy-get'){
+            taxonomies = await dataService.getContentByType('taxonomy');
+            res.send({data : taxonomies});
+            return;
+        }
 
+      let id = req.url.substring(req.url.lastIndexOf("/") + 1);
+
+      if (id) {
+        let taxonomy = await dataService.getContentById(id);
+        res.send(taxonomy.data);
+
+      }
+
+    });
+  },
+};
