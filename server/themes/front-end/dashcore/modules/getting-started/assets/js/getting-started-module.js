04be5149e83d852a6ff74f9ef71001dce6c7b869
lane711 <ldc0618@gmail.com>
2022-02-18 08:02:19 -0800

    home page updates

--- server/themes/front-end/dashcore/modules/getting-started/assets/js/getting-started-module.js
+++
@@ -1,8 +0,0 @@
-// JS File for Module: getting-started
-$(document).ready(async function () {
-    $('#copy-clone').click(function(){
-        let text = $("#clone-text").val();
-        navigator.clipboard.writeText(text);
-$(".copy-success").show();
-    })
-});
