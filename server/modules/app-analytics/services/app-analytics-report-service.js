75b3e4e8ebd466fe19be1d7b96f076bfa1a38144
lane711 <ldc0618@gmail.com>
2022-05-10 08:02:15 -0700

    add optin report

--- server/modules/app-analytics/services/app-analytics-report-service.js
+++ server/modules/app-analytics/services/app-analytics-report-service.js
@@ -3,9 +3,10 @@ var dataService = require("../../../services/data.service");
 var dalService = require("../../../services/dal.service");
 var emitterService = require("../../../services/emitter.service");
 var globalService = require("../../../services/global.service");
-var moment = require('moment');
+var moment = require("moment");
 var axios = require("axios");
 var axiosInstance;
+const _ = require("lodash");
 
 module.exports = appAnalyticsReportService = {
   getAggregates: async function (sessionID) {
@@ -14,12 +15,15 @@ module.exports = appAnalyticsReportService = {
       sessionID
     );
 
-    contents.forEach(log => {
-      if(log.data.firstSeenOn && log.data.lastSeenOn){
-        let age = moment(log.data.firstSeenOn).diff(log.data.lastSeenOn,'days');
+    contents.forEach((log) => {
+      if (log.data.firstSeenOn && log.data.lastSeenOn) {
+        let age = moment(log.data.firstSeenOn).diff(
+          log.data.lastSeenOn,
+          "days"
+        );
         log.data.age = age;
-      }else{
-        log.data.age='?'
+      } else {
+        log.data.age = "?";
       }
     });
 
@@ -28,5 +32,24 @@ module.exports = appAnalyticsReportService = {
     data.installs = contents;
     return data;
   },
+
+  getOptins: async function (sessionID) {
+    let contents = await dataService.getContentByType(
+      "app-analytics",
+      sessionID
+    );
+    let optins = [];
+
+    contents.map((log) => {
+      if (log.data.emailOptin) {
+        optins.push(log.data.email);
+      }
+    });
+
+    const distinctOptins = _.uniqBy(optins, function (e) {
+      return e;
+    });
+
+    return distinctOptins;
+  },
 };
- 
