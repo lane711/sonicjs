e129873148f68ebf635f444b27d2b19c0ca1082b
Lane Campbell <ldc0618@gmail.com>
2022-06-13 20:13:02 -0700

    event module details

--- server/modules/event/services/event-main-service.js
+++ server/modules/event/services/event-main-service.js
@@ -1,6 +1,8 @@
 var dataService = require('../../../services/data.service');
 var emitterService = require('../../../services/emitter.service');
 var globalService = require('../../../services/global.service');
+var moment = require("moment");
+var _ = require("underscore");
 
 module.exports = eventMainService = {
 
@@ -13,8 +15,49 @@ module.exports = eventMainService = {
                 await moduleService.processModuleInColumn(options);
             }
 
+            emitterService.on("postModuleGetData", async function (options) {
+                if (options.shortcode.name === "EVENT") {
+                  await eventMainService.getViewData(options);
+                }
+              });
+
         });
     },
+
+    getViewData: async function (options) {
+
+
+        let id = options.shortcode.properties.id;
+        let groupId = options.req.content ? options.req.content.id : null;
+    
+        let announcements = await dataService.getContentByTypeAndGroup(
+            'event',
+            groupId,
+            options.req.sessionID
+          );
+    
+    
+    
+        // let announcements = await dataService.getContentByContentType(
+        //   "announcement"
+        // );
+    
+        const now = new Date().getTime();
+    
+        announcements = _.sortBy(announcements, function (p) {
+          return p.data.dateTime;
+        });
+    
+        announcements.map((a) => {
+          a.data.dateFormatted = moment(a.data.dateTime).format("MMMM Do YYYY, h:mm a");
+        });
+    
+        options.viewModel.items = announcements;
+    
+        //emit so we can talley votes
+        await emitterService.emit("postModuleGetData2", options);
+    
+      },
 
 }
 
