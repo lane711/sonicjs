5b658508348f424f38b94efdb7929b25aaf7e093
Lane Campbell <ldc0618@gmail.com>
2022-06-30 11:28:51 -0700

    update admin view paths for registration

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -44,26 +44,26 @@ module.exports = authService = {
       res.send({ user: req.user });
     });
 
-    app.get("/register", async function (req, res) {
-      let data = { registerMessage: "<b>user</b>" };
-      res.render("admin/shared-views/user-register", {
-        layout: `front-end/${frontEndTheme}/login.hbs`,
-        data: data,
-      });
-      return;
-    });
+    // app.get("/register", async function (req, res) {
+    //   let data = { registerMessage: "<b>user</b>" };
+    //   res.render("/server/admin/shared-views/user-register", {
+    //     layout: `front-end/${frontEndTheme}/login.hbs`,
+    //     data: data,
+    //   });
+    //   return;
+    // });
 
-    app.post("/register", async function (req, res) {
-      let email = req.body.email;
-      let password = req.body.password;
-      let passwordConfirm = req.body.passwordConfirm;
+    // app.post("/register", async function (req, res) {
+    //   let email = req.body.email;
+    //   let password = req.body.password;
+    //   let passwordConfirm = req.body.passwordConfirm;
 
-      let newUser = await userService.registerUser(email, password);
+    //   let newUser = await userService.registerUser(email, password);
 
-      let message = encodeURI(`Account created successfully. Please login`);
-      res.redirect(`/login?message=${message}`); // /admin will show the login
-      return;
-    });
+    //   let message = encodeURI(`Account created successfully. Please login`);
+    //   res.redirect(`/login?message=${message}`); // /admin will show the login
+    //   return;
+    // });
 
     app.get("/register-admin", async function (req, res) {
       if (globalService.isAdminUserCreated == true) {
@@ -77,8 +77,8 @@ module.exports = authService = {
         data.message = parsedQs.message;
       }
 
-      res.render("admin/shared-views/admin-register", {
-        layout: `front-end/${frontEndTheme}/login.hbs`,
+      res.render("server/themes/admin/shared-views/admin-register", {
+        layout: `${appRoot.path}/server/themes/admin/shared-views/login.hbs`,
         data: data,
       });
       return;
@@ -114,10 +114,12 @@ module.exports = authService = {
 
     app.get("/register-admin-optin", async function (req, res) {
       let data = { email: req.app.get("optinEmail") };
-      res.render("admin/shared-views/admin-register-optin", {
-        layout: `front-end/${frontEndTheme}/login.hbs`,
+
+      res.render("server/themes/admin/shared-views/admin-register-optin", {
+        layout: `${appRoot.path}/server/themes/admin/shared-views/login.hbs`,
         data: data,
       });
+
       return;
     });
 
@@ -255,7 +257,7 @@ module.exports = authService = {
       if (options.req.url === "/register") {
         options.req.isRequestAlreadyHandled = true;
         let data = { registerMessage: "<b>admin</b>" };
-        options.res.render("admin/shared-views/admin-register", {
+        options.res.render("server/themes/admin/shared-views/admin-register", {
           layout: `front-end/${frontEndTheme}/login.handlebars`,
           data: data,
         });
