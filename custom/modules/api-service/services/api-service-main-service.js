6932f00abc8b05aa25276f9289a1011e4b98b7d5
Lane Campbell <ldc0618@gmail.com>
2022-12-20 08:44:55 -0800

    add doc cache

--- custom/modules/api-service/services/api-service-main-service.js
+++ custom/modules/api-service/services/api-service-main-service.js
@@ -1,7 +1,8 @@
 var dataService = require("../../../../server/services/data.service");
 var emitterService = require("../../../../server/services/emitter.service");
 var globalService = require("../../../../server/services/global.service");
-const jsDocData = require("../../../../docs/json/jsdoc-ast.json");
+const NodeCache = require("node-cache");
+const docCache = new NodeCache();
 
 module.exports = apiServiceMainService = {
   startup: async function () {
@@ -20,17 +21,26 @@ module.exports = apiServiceMainService = {
   },
 
   getJSDocData: async function (data) {
-    const modules = jsDocData.filter((m) => m.kind === "module");
-    // console.log("modules from jsdoc json", modules);
-    var apis = [];
-    for (module of modules) {
-      apis.push({
-        fileName: module.meta.filename,
-        name: module.name,
-        description: module.description,
-      });
+    var apiData = [];
+
+    let result = docCache.get("apiData");
+
+    if (result) {
+      apiData = result;
+    } else {
+      jsDocData = require("../../../../docs/json/jsdoc-ast.json");
+      const modules = jsDocData.filter((m) => m.kind === "module");
+
+      for (module of modules) {
+        apiData.push({
+          fileName: module.meta.filename,
+          name: module.name,
+          description: module.description,
+        });
+      }
+      success = docCache.set("apiData", apiData, 12 * 60 *60); //12 hours
     }
 
-    data.viewModel.data.apis = apis;
+    data.viewModel.data.apis = apiData;
   },
 };
