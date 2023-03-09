7697a54bf47b61cb296fd680d78d362d95ca7a3f
Lane Campbell <ldc0618@gmail.com>
2023-03-08 17:17:30 -0800

    proposal module ui

--- server/modules/proposal/services/proposal-main-service.js
+++ server/modules/proposal/services/proposal-main-service.js
@@ -1,20 +1,28 @@
-var dataService = require('../../../services/data.service');
-var emitterService = require('../../../services/emitter.service');
-var globalService = require('../../../services/global.service');
+var dataService = require("../../../services/data.service");
+var emitterService = require("../../../services/emitter.service");
+var globalService = require("../../../services/global.service");
+var helperService = require("../../../services/helper.service");
 
 module.exports = proposalMainService = {
+  startup: async function () {
+    emitterService.on("beginProcessModuleShortCode", async function (options) {
+      if (options.shortcode.name === "PROPOSAL") {
+        options.moduleName = "proposal";
+        await moduleService.processModuleInColumn(options);
+      }
+    });
 
-    startup: async function () {
-        emitterService.on('beginProcessModuleShortCode', async function (options) {
+    emitterService.on("postModuleGetData", async function (options) {
+      if (options.shortcode.name === "PROPOSAL") {
+        await proposalMainService.getData(options);
+      }
+    });
+  },
 
-            if (options.shortcode.name === 'PROPOSAL') {
+  getData: async function (options) {
 
-                options.moduleName = 'proposal';
-                await moduleService.processModuleInColumn(options);
-            }
-
-        });
-    },
-
-}
-
+    for (let section of options.viewModel.data.sections) {
+        section.slug = helperService.slugify(section.title);
+    }
+  },
+};
