260b0126769be9a430e35ea64e870a8491030819
Lane Campbell <ldc0618@gmail.com>
2023-06-03 07:47:58 -0700

    clean up

--- src/cms/admin/pages/api.tsx
+++ src/cms/admin/pages/api.tsx
@@ -2,17 +2,9 @@ import { getById, getDataListByPrefix, putData } from "../../data/data";
 import { Layout } from "../theme";
 
 export async function loadApis(ctx) {
-  // await ctx.env.KVDATA.put("host::sites::site2", JSON.stringify({ title: "The Blue Website" }));
-
-  // console.log("mods ctx", ctx);
-  // console.log("ctx KVDATA", ctx.env.KVDATA);
 
   const sites = await getDataListByPrefix(ctx.env.KVDATA, "host::sites");
-  // console.log("sites -->", sites);
 
-  //   const data = await getDataListByPrefix(ctx.env.KVDATA, "host::sites::");
-
-  //   console.log("data site", data[0]);
   let contentTypeApis = [
     "/api/content-type/site1::content-type::blog",
   ];
@@ -22,16 +14,6 @@ export async function loadApis(ctx) {
     "/api/content?contentType=blog",
     "/api/content?meta=true",
   ];
-
-  // let data = [{id:"123",title:'ipsum'}];
-  const list = contentTypeApis.map((item) => {
-    console.log("data site", item);
-
-    return {
-      title: item,
-      path: item,
-    };
-  });
 
   return <Top contentTypeApis={contentTypeApis} contentApis={contentApis} screenTitle="APIs" />;
 }
@@ -71,16 +53,3 @@ export const Top = (props: {
   );
 };
 
-export async function loadSite(ctx, id) {
-  const data = await getById(ctx.env.KVDATA, "host::sites::orange-site");
-
-  console.log("data site", data);
-  const list = data.map((item) => {
-    return {
-      title: item.title,
-      path: `/admin/content-types/${item.name}`,
-    };
-  });
-
-  return <Top items={list} screenTitle="Sites" />;
-}
