254d6e853f6ac641788d47eae2fc197095c3bad3
Lane <>
2023-06-13 07:07:30 -0700

    d1 setup

--- src/cms/api/search.ts
+++ src/cms/api/search.ts
@@ -1,15 +1,4 @@
 import { Hono } from "hono";
-import { getForm, loadForm } from "../admin/forms/form";
-import {
-  getById,
-  getContentType,
-  getContentTypes,
-  getDataByPrefix,
-  getDataListByPrefix,
-  putData,
-  saveContent,
-  saveContentType,
-} from "../data/kv-data";
 import { Bindings } from "../types/bindings";
 
 const search = new Hono<{ Bindings: Bindings }>();
@@ -18,7 +7,13 @@ search.get("/", async (ctx) => {
 
   console.log('searching data with d1')
 
-  return ctx.json({ok:'ok'});
+  const { results } = await ctx.env.D1DATA.prepare(
+    "SELECT * FROM Customers"
+  )
+    .all();
+    return ctx.json(results);
+  
+
 });
 
 export { search };
