ec5b0cad9ff10c1a35f995a039e8c55eec55d186
Corey Jepperson <corey.jepperson@gmail.com>
2023-10-06 11:01:52 -0500

    remove test dropdown

--- src/cms/admin/theme.tsx
+++ src/cms/admin/theme.tsx
@@ -54,36 +54,6 @@ export const Layout = (props: {
         <link href="/public/css/admin.css" rel="stylesheet" />
       </head>
       <body>
-        <div class="dropdown">
-          <a
-            class="btn btn-secondary dropdown-toggle"
-            href="#"
-            role="button"
-            id="dropdownMenuLink"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown link
-          </a>
-
-          <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <li>
-              <a class="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
-        </div>
         <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
           <button
             class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
