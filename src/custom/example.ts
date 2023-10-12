1c6c45db534298b6b66a6c9fffe26549dc83917c
Lane <ldc0618@gmail.com>
2023-10-12 10:34:53 -0700

    post without caching

--- src/custom/example.ts
+++ src/custom/example.ts
@@ -195,6 +195,51 @@ example.get("/blog-posts2", async (ctx) => {
   return ctx.json({ ...data, executionTime });
 });
 
+example.get("/blog-posts3", async (ctx) => {
+  const start = Date.now();
+  var params = qs.parse(ctx.req.query());
+  const d1 = getD1Binding(ctx);
+
+  let limit = params.limit ? params.limit : 10;
+  let offset = params.offset ? params.offset : 0;
+
+  const { results } = await d1
+    .prepare(
+      `
+    SELECT
+    posts.id,
+    posts.title,
+    posts.updatedOn,
+    substr(posts.body, 0, 20) as body,
+    users.firstName || ' ' || users.lastName as author,
+    count(comments.id) as commentCount,
+    categories.title as category,
+    COUNT() OVER() as total
+    FROM posts
+    left outer join users
+    on posts.userid = users.id
+    left outer join comments
+    on comments.postId = posts.id
+    left outer join categoriesToPosts
+    on categoriesToPosts.postId = posts.id
+    left outer join categories
+    on categoriesToPosts.categoryId = categories.id
+    group by posts.id
+    order by posts.updatedOn desc
+    limit 10
+    offset 0
+    `
+    )
+    .all();
+
+  results.source = "d1";
+
+  const end = Date.now();
+  const executionTime = end - start;
+
+  return ctx.json({ ...results, executionTime });
+});
+
 example.get("/blog-posts/:id", async (ctx) => {
   const start = Date.now();
   const id = ctx.req.param("id");
