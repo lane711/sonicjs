c626a284d00f1e9168ef952be8d52eef4cbbfd8f
Lane <ldc0618@gmail.com>
2023-10-20 16:36:10 -0700

    add cache status

--- src/cms/api/status.ts
+++ src/cms/api/status.ts
@@ -5,6 +5,7 @@ import { getById } from "../data/kv-data";
 import axios from "axios";
 import { datadogLogs } from "@datadog/browser-logs";
 import { log } from "../util/logger";
+import { addToInMemoryCache, getFromInMemoryCache } from "../data/cache";
 
 const status = new Hono<{ Bindings: Bindings }>();
 
@@ -37,10 +38,20 @@ status.get("/", async (ctx) => {
 
   //kv
   try {
-    const allCacheItems = await getById(ctx.env.KVDATA, "1");
+    const allKvItems = await getById(ctx.env.KVDATA, "1");
     status.kv = "ok";
   } catch (error) {
     status.kv = "error: " + error;
+  }
+
+  //cache
+  try {
+    const now = new Date().getTime().toString();
+    const newCacheItem = await addToInMemoryCache(now, { status: "ok" });
+    const itemFromCache = await getFromInMemoryCache(now);
+    status.cache = itemFromCache[0].data.status;
+  } catch (error) {
+    status.cache = "error: " + error;
   }
 
   //env
@@ -49,10 +60,10 @@ status.get("/", async (ctx) => {
     var safeOutput = {};
     for (var prop in ctx.env) {
       if (Object.prototype.hasOwnProperty.call(ctx.env, prop)) {
-        safeOutput[prop] = '[redacted]'
+        safeOutput[prop] = "[redacted]";
       }
       status.env = safeOutput;
-  }
+    }
   } catch (error) {
     status.env = "error: " + error;
   }
@@ -68,22 +79,22 @@ status.get("/log", async (ctx) => {
 
 status.get("/geo", (ctx) => {
   let html_content = "";
-    let html_style =
-      "body{padding:6em; font-family: sans-serif;} h1{color:#f6821f;}";
+  let html_style =
+    "body{padding:6em; font-family: sans-serif;} h1{color:#f6821f;}";
 
-    html_content += "<p> Colo: " + ctx.req.cf.colo + "</p>";
-    html_content += "<p> Country: " + ctx.req.cf.country + "</p>";
-    html_content += "<p> City: " + ctx.req.cf.city + "</p>";
-    html_content += "<p> Continent: " + ctx.req.cf.continent + "</p>";
-    html_content += "<p> Latitude: " + ctx.req.cf.latitude + "</p>";
-    html_content += "<p> Longitude: " + ctx.req.cf.longitude + "</p>";
-    html_content += "<p> PostalCode: " + ctx.req.cf.postalCode + "</p>";
-    html_content += "<p> MetroCode: " + ctx.req.cf.metroCode + "</p>";
-    html_content += "<p> Region: " + ctx.req.cf.region + "</p>";
-    html_content += "<p> RegionCode: " + ctx.req.cf.regionCode + "</p>";
-    html_content += "<p> Timezone: " + ctx.req.cf.timezone + "</p>";
+  html_content += "<p> Colo: " + ctx.req.cf.colo + "</p>";
+  html_content += "<p> Country: " + ctx.req.cf.country + "</p>";
+  html_content += "<p> City: " + ctx.req.cf.city + "</p>";
+  html_content += "<p> Continent: " + ctx.req.cf.continent + "</p>";
+  html_content += "<p> Latitude: " + ctx.req.cf.latitude + "</p>";
+  html_content += "<p> Longitude: " + ctx.req.cf.longitude + "</p>";
+  html_content += "<p> PostalCode: " + ctx.req.cf.postalCode + "</p>";
+  html_content += "<p> MetroCode: " + ctx.req.cf.metroCode + "</p>";
+  html_content += "<p> Region: " + ctx.req.cf.region + "</p>";
+  html_content += "<p> RegionCode: " + ctx.req.cf.regionCode + "</p>";
+  html_content += "<p> Timezone: " + ctx.req.cf.timezone + "</p>";
 
-    let html = `<!DOCTYPE html>
+  let html = `<!DOCTYPE html>
       <head>
         <title> Geolocation: Hello World </title>
         <style> ${html_style} </style>
