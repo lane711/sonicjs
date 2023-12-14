272ff47c52b4b6212a31c345206827780d2cda04
Corey Jepperson <corey.jepperson@gmail.com>
2023-12-14 14:07:28 -0600

    test fixes

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   testEnvironment: "miniflare",
   testMatch: ["**/test/**/*.+(ts|tsx)", "**/src/**/(*.)+(spec|test).+(ts|tsx)"],
+  transformIgnorePatterns: ["node_modules/(?!(lucia|@lucia-auth)/)"],
   transform: {
-    "^.+\\.(ts|tsx)$": [
+    "^.+\\.(js|ts|tsx)$": [
       "esbuild-jest",
       {
         sourcemap: true,
