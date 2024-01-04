bc6a6a85aef57ee95dc2b9ab0b825a0ac0b7f79d
Corey Jepperson <corey.jepperson@gmail.com>
2024-01-04 16:56:47 -0600

    config

--- src/cms/api/forms.ts
+++ src/cms/api/forms.ts
@@ -11,7 +11,7 @@ export function getForm(ctx: AppContext, table) {
   const schema = getSchemaFromTable(table);
   console.log("schema", schema);
   const config = apiConfig.find((tbl) => tbl.table === table);
-
+  console.log("config", config);
   for (var field in schema) {
     const formField = getField(field);
     const metaType = config.fields?.[field]?.type || "auto";
