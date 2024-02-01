7397246deeb03ad39b562a1b7f10c28cc04ec8eb
Corey Jepperson <corey.jepperson@gmail.com>
2024-02-01 09:25:18 -0600

    comment migration

---
+++ migrations/0004_amusing_midnight.sql
@@ -0,0 +1,21 @@
+/*
+You're trying to delete PRIMARY KEY(categoryId,postId) from 'categoriesToPosts' table
+SQLite does not supportprimary key deletion from existing table
+You can do it in 3 steps with drizzle orm:
+ - create new mirror table table without pk, rename current table to old_table, generate SQL
+ - migrate old data from one table to another
+ - delete old_table in schema, generate sql
+
+or create manual migration like below:
+
+ALTER TABLE table_name RENAME TO old_table;
+CREATE TABLE table_name (
+	column1 datatype [ NULL | NOT NULL ],
+	column2 datatype [ NULL | NOT NULL ],
+	...
+	PRIMARY KEY (pk_col1, pk_col2, ... pk_col_n)
+ );
+INSERT INTO table_name SELECT * FROM old_table;
+
+Due to that we don't generate migration automatically and it has to be done manually
+*/
