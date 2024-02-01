b865fc086de367b0110e25eda5e76ec15bb7ce67
Rhamses <rhamses.soares@gmail.com>
2024-02-01 20:46:33 -0300

    create new env variables to support signed urls and public bucket url

--- src/cms/types/bindings.d.ts
+++ src/cms/types/bindings.d.ts
@@ -6,6 +6,11 @@ export type Bindings = {
   D1DATA: D1Database;
   R2_BUCKET: R2_BUCKET;
   __D1_BETA__D1DATA: D1Database;
+  BUCKET_NAME: string;
+  BUCKET_ACCESS_KEY_ID: string;
+  BUCKET_SECRET_ACCESS_KEY: string;
+  BUCKET_CUSTOM_DOMAIN: string;
+  ACCOUNT_ID: string;
 };
 
 declare global {
