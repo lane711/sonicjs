0787251f1a8c890c73aff957e264f60a81cd7454
Lane <ldc0618@gmail.com>
2024-02-09 09:12:16 -0800

    show credential on demo domain

--- src/cms/admin/pages/login.tsx
+++ src/cms/admin/pages/login.tsx
@@ -1,5 +1,9 @@
 import { Head, Script, ToggleTheme } from "../theme";
-export const Login = (props: { children?: string; screenTitle?: string }) => {
+export const Login = (props: {
+  children?: string;
+  screenTitle?: string;
+  showDemoCredentials?: boolean;
+}) => {
   return (
     <html lang="en" data-bs-theme="auto">
       <Head />
@@ -17,7 +21,11 @@ export const Login = (props: { children?: string; screenTitle?: string }) => {
 
         <div class="container-fluid">
           <div class="row">
-            <main class="col-6 offset-3 px-md-4 py-md-4">
+            <main class="col-4 offset-4 px-md-4 py-md-5">
+              <h1>Login</h1>
+              <DemoCredentials
+                showDemoCredentials={props.showDemoCredentials}
+              ></DemoCredentials>
               <p class="text-danger" id="login-errors"></p>
               <form id="formio-login"></form>
               {props.children}
@@ -31,8 +39,41 @@ export const Login = (props: { children?: string; screenTitle?: string }) => {
   );
 };
 
+export const DemoCredentials = (props: { showDemoCredentials?: boolean }) => {
+  if (props.showDemoCredentials) {
+    return (
+      <div class="alert alert-primary d-flex align-items-center" role="alert">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2"
+          viewBox="0 0 16 16"
+          role="img"
+          aria-label="Warning:"
+        >
+          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+        </svg>
+        <div>
+          <b>Demo Login:</b>
+          <br />
+          demo@demo.com
+          <br />
+          sonicjs!
+        </div>
+      </div>
+    );
+  }
+  return (
+    <></>
+  )
+};
+
 export async function loadLogin(ctx) {
-  return <Login screenTitle="Login" />;
+  const demoDomain= ctx.env.demo_domain;
+  const showDemoCredentials = demoDomain && ctx.req.url.indexOf(demoDomain) > 0 ? true : false;
+  return <Login screenTitle="Login" showDemoCredentials={showDemoCredentials} />;
 }
 
 export const Setup = (props: { children?: string; screenTitle?: string }) => {
