832e19ca6f0455983a2012da81fe2c9b157b0205
Lane <ldc0618@gmail.com>
2024-03-27 06:56:08 -0700

    consume contact

--- src/custom/rife-player-api.test.ts
+++ src/custom/rife-player-api.test.ts
@@ -12,7 +12,7 @@ const toJson = function (json) {
 };
 
 const ctx = {
-  env: { KVDATA: KVDATA, D1DATA: __D1_BETA__D1DATA },
+  env: { KVDATA: KVDATA, D1DATA: __D1_BETA__D1DATA, APIKEY:'123' },
   json: toJson,
   user: { id: 'fromtest' },
   _var: { user: { userId: 'abc123' } }
@@ -46,7 +46,24 @@ it('rp programs', async () => {
   let body = await res.json();
   expect(body.data.length).toBe(20);
   expect(body.data[0].frequencies).toBeInstanceOf(Array);
+});
 
+it('contact post should (insert) and should return 204', async () => {
+  await createContactTable(ctx);
+  let payload = JSON.stringify({
+    data: { firstName: 'Joe' },
+    token: ctx.env.APIKEY
+  });
+  let req = new Request('http://localhost/v2/contact-submit', {
+    method: 'POST',
+    body: payload,
+    headers: { 'Content-Type': 'application/json' }
+  });
+  let res = await app.fetch(req, ctx.env);
+  expect(res.status).toBe(201);
+  let body = await res.json();
+  expect(body.id.length).toBeGreaterThan(1);
+  expect(body.firstName).toBe('Joe');
 });
 
 async function createTestTable(ctx) {
@@ -68,6 +85,27 @@ async function createTestTable(ctx) {
       );
       `);
   console.log('creating programs table end');
+
+  return db;
+}
+
+async function createContactTable(ctx) {
+  const db = drizzle(ctx.env.D1DATA);
+  console.log('creating contacts table start');
+  await db.run(sql`
+  CREATE TABLE contacts (
+    id text PRIMARY KEY NOT NULL,
+    firstName text,
+    lastName text,
+    company text,
+    email text,
+    phone text,
+    message text,
+    createdOn integer,
+    updatedOn integer
+      );
+      `);
+  console.log('creating contacts table end');
 
   return db;
 }
--- src/custom/rife-player-api.ts
+++ src/custom/rife-player-api.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { getPrograms } from './rife-player-data';
+import { insertRecord } from '../cms/data/data';
 
 const rifePlayerApi = new Hono();
 
@@ -10,6 +11,23 @@ rifePlayerApi.get('/', async (ctx) => {
 rifePlayerApi.get('/programs', async (ctx) => {
   const data = await getPrograms(ctx);
   return ctx.json(data);
+});
+
+rifePlayerApi.post('/contact-submit', async (ctx) => {
+  const payload = await ctx.req.json();
+
+  const token = payload.token;
+  if(token !== ctx.env.APIKEY){
+    return ctx.text('Unauthorized', 401);
+  }
+
+  payload.table = 'contacts';
+
+  const record = await insertRecord(ctx.env.D1DATA, ctx.env.KVDATA, payload);
+
+  //send email confirmations
+
+  return ctx.json(record.data, record.code);
 });
 
 export { rifePlayerApi };
