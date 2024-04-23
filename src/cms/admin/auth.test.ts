2d0e3cbe0107f56d94c84a38a2e8cd97a220fd32
Lane <ldc0618@gmail.com>
2024-04-23 08:13:05 -0700

    allow user registration via api

--- src/cms/admin/auth.test.ts
+++ src/cms/admin/auth.test.ts
@@ -1,5 +1,6 @@
 import app from '../../server';
 import { insertD1Data } from '../data/d1-data';
+import { getRecords } from '../data/data';
 import {
   createCategoriesTestTable1,
   createUserAndGetToken,
@@ -98,6 +99,75 @@ describe('admin should be restricted', () => {
     expect(userResponse.data.id).toBe(user.id);
   });
 
+  it('register user via the api', async () => {
+    await createUserTestTables(ctx);
+  
+    const account = {
+      data: {
+        firstName: '',
+        lastName: '',
+        role: 'user',
+        email: 'a@a.com',
+        password: '12341234',
+        table: 'users'
+      }
+    };
+  
+    let req = new Request(`http://localhost/v1/auth/users/setup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(account)
+    });
+    let res = await app.fetch(req, ctx.env);
+  
+    //by default users can't register on their own
+    expect(res.status).toBe(200);
+    let body = await res.json();
+  
+    //check that user exists
+    let users = await getRecords(
+      ctx,
+      'users',
+      undefined,
+      '/users-url',
+      'd1',
+      undefined
+    );
+    expect(users.data.length).toBe(1);
+  
+    //add another
+    account.data.email = 'b@b.com';
+  
+    let req2 = new Request(`http://localhost/v1/auth/users/setup`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(account)
+    });
+    let res2 = await app.fetch(req2, ctx.env);
+  
+
+    expect(res2.status).toBe(401);
+
+    // if create user (register) is allowed, you can uncomment the below
+    // let body2 = await res2.json();
+  
+    // //check that user exists
+    // let users2 = await getRecords(
+    //   ctx,
+    //   'users',
+    //   undefined,
+    //   '/users-url',
+    //   'd1',
+    //   undefined
+    // );
+    // expect(users2.data.length).toBe(2);
+    // expect(users2.data[1].email).toBe('b@b.com');
+  
+  });
   // it('admin can see all user records', async () => {
   //   const user = await createUserAndGetToken(app, ctx);
   //   // const user2 = await createUserAndGetToken(app, ctx, 'b@b.com', 'password', 'editor');
