8cb12ee6e3a9b6f9f15e2a450a90a54bb129450b
Lane <ldc0618@gmail.com>
2024-05-03 14:17:10 -0700

    add api logs

--- src/cms/admin/auth.ts
+++ src/cms/admin/auth.ts
@@ -23,6 +23,7 @@ import {
   getOperationCreateResult,
   hasUser
 } from '../auth/auth-helpers';
+import { log } from '../util/logger';
 
 const authAPI = new Hono<{ Bindings: Bindings; Variables: Variables }>();
 authAPI.use('*', async (ctx, next) => {
@@ -203,8 +204,11 @@ authAPI.post(`/users/:setup?`, async (ctx) => {
   content.table = 'users';
   // HACK: need a better fix - this should only apply when the admin is first creating their account
   content.data.role = content.data.role ?? 'admin';
-  
+
   delete content.data.submit;
+
+  log(ctx, { level: 'info', method: '/users/setup', content });
+
   if (
     content.data?.confirmPassword &&
     content.data?.confirmPassword !== content.data?.password
@@ -398,10 +402,15 @@ authAPI.put(`/users/:id`, async (ctx) => {
 
 authAPI.post('/login', async (ctx) => {
   const content = await ctx.req.json();
+
+  log(ctx, { level: 'info', method: '/login', content });
+
   return await login({ ctx, content });
 });
 
 authAPI.get('/logout', async (ctx) => {
+  log(ctx, { level: 'info', method: '/logout' });
+
   return await logout(ctx);
 });
 
