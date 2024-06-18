7456df98966fc997ae764b21dbebf7f8af93065a
Lane <ldc0618@gmail.com>
2024-06-17 17:57:02 -0700

    keys loading

--- src/cms/admin/theme.tsx
+++ src/cms/admin/theme.tsx
@@ -74,6 +74,7 @@ export const Script = () => {
       <script src='/public/js/grid.js'></script>
       <script src='/public/js/grid-in-memory-cache.js'></script>
       <script src='/public/js/grid-kv-cache.js'></script>
+      <script src='/public/js/grid-keys-cache.js'></script>
       <script
         src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'
         integrity='sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL'
