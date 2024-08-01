66fe6865e261994e2128b7e8b52287031cbd0e39
Lane <ldc0618@gmail.com>
2024-08-01 14:20:09 -0700

    log env

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
 export type AppContext = Context<{ Bindings: Bindings; Variables: Variables }>;
 
 app.use('*', async (ctx, next) => {
+  console.log(ctx.env)
   console.log('auth disabled?' , ctx.env.disable_auth)
   if(ctx.env.disable_auth = true){
     await next();
