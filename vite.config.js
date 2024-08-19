3e29743c227c3c5ad44499a9927637f82d4dba0a
Lane <ldc0618@gmail.com>
2024-08-19 16:56:37 -0700

    rename build to worker

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, "src/index.ts"),
-      name: "index",
+      name: "soncisjs",
       // the proper extensions will be added
-      fileName: "index",
+      fileName: "worker",
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
