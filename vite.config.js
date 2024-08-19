ae513787e321fdfd5a0006d6d68c96dae66caefb
Lane <ldc0618@gmail.com>
2024-08-19 16:43:32 -0700

    add build

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "index",
+      // the proper extensions will be added
+      fileName: "index",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {},
+      },
+    },
+  },
+});
