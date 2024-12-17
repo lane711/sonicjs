220f50245832133bab7e95c280c445af9e9ca7a7
Lane C <ldc0618@gmail.com>
2024-12-16 16:15:52 -0800

    clean up

--- src/pages/api/v1/[table].testXXX.ts
+++ src/pages/api/v1/[table].testXXX.ts
@@ -1,12 +1,14 @@
-
 import { expect, test } from 'vitest'
 import { GET } from './[table]'
 
-// test('adds 1 + 2 to equal 3', () => {
-//   const result = GET({
-//     params:
-//     {'table': 'test'}
-//   });
+test('adds 1 + 2 to equal 3', () => {
+  const result = GET({
+    site: 'exampleSite',
+    generator: 'exampleGenerator',
+    url: 'exampleUrl',
+    props: {},
+    params: {'table': 'test'}
+  });
 
-//   expect(result)).toBe({})
-// })
+  expect(result).toEqual({})
+})
