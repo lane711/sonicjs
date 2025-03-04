870d980292b2e3fce9a84d609c60a1e247e5b145
Lane C <ldc0618@gmail.com>
2025-03-04 14:47:49 -0800

    create user test

--- e2e/api-admin.spec.ts
+++ e2e/api-admin.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { purgeE2eTestData } from "@services/e2e";
 
 const adminCredentials = {
   email: "demo@demo.com",
@@ -13,7 +14,7 @@ async function loginAsAdmin(request) {
     },
   });
   const { bearer } = await response.json();
-  console.log("bearer", bearer);
+  // console.log("bearer", bearer);
   return bearer;
 }
 
@@ -26,33 +27,32 @@ test("should allow admin to access /api/v1/users", async ({ request }) => {
   });
   expect(response.status()).toBe(200);
   const { data } = await response.json();
-  console.log(data);
+  // console.log(data);
   expect(Array.isArray(data)).toBe(true);
 });
 
 test("should allow admin to create a user", async ({ request }) => {
   const token = await loginAsAdmin(request);
-  const response = await request.post(`/api/v1/users`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    data: {
-      username: "newuser",
-      password: "password123",
-    },
-  });
+  const response = await createTestUser(request, token);
   expect(response.status()).toBe(201);
 });
 
 test("should allow admin to delete a user", async ({ request }) => {
   const token = await loginAsAdmin(request);
-  const response = await request.delete(`/api/v1/users/1`, {
+  
+  const createUserResponse = await createTestUser(request, token);
+  expect(createUserResponse.status()).toBe(201);
+  const { data } = await createUserResponse.json();
+
+  const response = await request.delete(`/api/v1/users/${data.id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
   expect(response.status()).toBe(200);
 });
+
+
 
 test("should allow admin to update a user", async ({ request }) => {
   const token = await loginAsAdmin(request);
@@ -61,8 +61,10 @@ test("should allow admin to update a user", async ({ request }) => {
       Authorization: `Bearer ${token}`,
     },
     data: {
-      username: "updateduser",
-      password: "newpassword123",
+      data: {
+        email: "e2e-updated@test.com",
+        password: "newpassword123abc",
+      },
     },
   });
   expect(response.status()).toBe(200);
@@ -78,3 +80,39 @@ test("should allow unauthenticated user to access /api/v1/posts", async ({
   expect(Array.isArray(data)).toBe(true);
   expect(data.length).toBeGreaterThan(0);
 });
+
+test.afterEach(async ({request}) => {
+  console.log('e2e cleanup...');
+
+  const token = await loginAsAdmin(request);
+  const response = await request.post(`/api/v1/test/e2e/cleanup`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  expect(response.status()).toBe(200);
+
+  console.log('Done with tests');
+  // ...
+});
+
+const createTestUser = async (request, token) => {
+  const response = await request.post(`/api/v1/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    data: {
+      data: {
+        email: "e2e!!@test.com",
+        password: "newpassword123abc",
+      },
+    },
+  });
+
+  return response;
+}
+// test.afterAll(async (context ) => {
+//   const data = await purgeE2eTestData(context.locals.runtime.env.D1, "users", "email");
+
+// });
