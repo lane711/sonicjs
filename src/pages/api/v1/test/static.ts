b3f0f3f60f9f72999a12fde3777c49201dc284cb
Lane Campbell <ldc0618@gmail.com>
2025-04-15 22:21:07 -0700

    return 200 on static

--- src/pages/api/v1/test/static.ts
+++ src/pages/api/v1/test/static.ts
@@ -1,3 +1,4 @@
+import { return200 } from "@services/return-types";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async (context) => {
@@ -14,14 +15,15 @@ export const GET: APIRoute = async (context) => {
         { id: 10, title: 'Tenth Blog Post', content: 'This is the content of the tenth blog post.' },
     ];
 
+    return return200(blogPosts);
 
-    return new Response(JSON.stringify(blogPosts), {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Max-Age': '86400', // 24 hours
-        },
-    });
+    // return new Response(JSON.stringify(blogPosts), {
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Access-Control-Allow-Origin': '*',
+    //         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    //         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    //         'Access-Control-Max-Age': '86400', // 24 hours
+    //     },
+    // });
 };
