4a515def79d4f4029adf848e9f6d8f897ac0989a
Lane Campbell <ldc0618@gmail.com>
2025-05-07 17:57:20 -0700

    cleanup

--- src/services/return-types.ts
+++ src/services/return-types.ts
@@ -7,22 +7,10 @@ const commonHeaders = {
 };
 
 export const return200 = (data = {}) => {
-
   return new Response(JSON.stringify(data), {
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Max-Age': '86400', // 24 hours
-    },
+    headers: commonHeaders,
     status: 200,
-});
-
-  // return new Response(JSON.stringify(data), {
-  //   headers: { ...commonHeaders },
-  //   status: 200,
-  // });
+  });
 };
 
 export const return201 = (data) => {
@@ -62,24 +50,19 @@ export const return404 = (message = "Not Found") => {
 export function return500(): Response;
 export function return500(message: string): Response;
 export function return500(object: object): Response;
-export function return500(messageOrObject: string | object = "Internal Server Error"): Response {
+export function return500(
+  messageOrObject: string | object = "Internal Server Error"
+): Response {
   if (typeof messageOrObject === "string") {
     return new Response(
       JSON.stringify({
         message: messageOrObject,
       }),
-      { status: 500 }
+      { headers: commonHeaders, status: 500 }
     );
   }
-  return new Response(
-<<<<<<< HEAD
-    JSON.stringify(messageOrObject),
-    { status: 500 }
-=======
-    JSON.stringify({
-      message,
-    }),
-    { headers: commonHeaders, status: 500 }
->>>>>>> main
-  );
+  return new Response(JSON.stringify(messageOrObject), {
+    headers: commonHeaders,
+    status: 500,
+  });
 }
