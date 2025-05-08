354c588386ae9d74fbfbe1d51602d679c164e107
Lane Campbell <ldc0618@gmail.com>
2025-05-07 17:28:55 -0700

    remove

--- migrations/0004_first_ender_wiggin.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `users` ADD `passwordExpired` boolean DEFAULT false;--> statement-breakpoint
-ALTER TABLE `users` ADD `passwordOTP` text;
--- migrations/0005_public_plazm.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `users` ADD `passwordExpiresOn` integer;--> statement-breakpoint
-ALTER TABLE `users` ADD `passwordOTPExpiresOn` integer;--> statement-breakpoint
-ALTER TABLE `users` DROP COLUMN `passwordExpired`;
