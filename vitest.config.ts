3d7b4db8ddf6f9541a5a5c0fe87c898cb35e13c7
mubrik <mubarakg4u@gmail.com>
2025-10-22 23:58:49 +0100

    exclude playright test files from vitest, remove GH action invalid npm run check

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,10 @@ export default getViteConfig({
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
+      // playwright test should be excluded from vitest, add a seperate playright test command later
+      './e2e',
+      './tests-examples',
+      // this file has only comments
       "./src/pages/api/v1/test/vars/set.test.ts"
     ]
   },
